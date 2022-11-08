e4e5bcc7aa537a4c44ea2ec3dca2c23b4aeed2b6
Mercy Ma <mercyblitz@gmail.com>
2022-11-08 10:26:58 +0800

    Update ReloadableResourceServiceMessageSource.java

--- microsphere-commons-i18n/src/main/java/io/github/microsphere/commons/i18n/ReloadableResourceServiceMessageSource.java
+++ microsphere-commons-i18n/src/main/java/io/github/microsphere/commons/i18n/ReloadableResourceServiceMessageSource.java
@@ -3,7 +3,7 @@ package io.github.microsphere.commons.i18n;
 import java.util.Set;
 
 /**
- * 可重加载 {@link ResourceServiceMessageSource}
+ * Reloadable {@link ResourceServiceMessageSource}
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
  * @since 1.0.0
@@ -11,17 +11,18 @@ import java.util.Set;
 public interface ReloadableResourceServiceMessageSource extends ResourceServiceMessageSource {
 
     /**
-     * 重载资源当 {@link #canReload(Iterable)} 为 <code>true</code>，默认调用 {@link #init()}
+     * Reload if {@link #canReload(Iterable)} returns <code>true</code>,
+     * The calling {@link #init()} as default
      */
     default void reload() {
         init();
     }
 
     /**
-     * 指定的资源列表能否重载
+     * Whether the specified resource list can be overloaded
      *
-     * @param changedResources 变化资源
-     * @return 默认支持，返回 <code>true</code>
+     * @param changedResources Changes in the resource
+     * @return Supported by default, returning <code>true<code>
      */
     default boolean canReload(Iterable<String> changedResources) {
         Set<String> resources = getResources();
