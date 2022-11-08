3a75ede2f47fb9433e213e4085ddf1b74bddb1b4
Mercy Ma <mercyblitz@gmail.com>
2022-11-08 10:33:18 +0800

    Update MessageUtilsTest.java

--- microsphere-commons-i18n/src/test/java/io/github/microsphere/commons/i18n/util/MessageUtilsTest.java
+++ microsphere-commons-i18n/src/test/java/io/github/microsphere/commons/i18n/util/MessageUtilsTest.java
@@ -28,24 +28,24 @@ public class MessageUtilsTest {
 
     @Test
     public void testGetLocalizedMessage() {
-        // 测试简体中文
+        // Testing Simplified Chinese
         // null
         assertEquals(null, MessageUtils.getLocalizedMessage(null));
-        // message 参数为 "a" 的情况，不包含模式 "{" "}"，返回原始内容
+        // If the message argument is "a", the pattern "{" "}" is not included, and the original content is returned
         assertEquals("a", MessageUtils.getLocalizedMessage("a"));
-        // "{a}" 为 Message Code 模板，其中 "a" 为 Message Code
+        // "{a}" is the Message Code template, where "a" is Message Code
         assertEquals("测试-a", MessageUtils.getLocalizedMessage("{a}"));
 
-        // 同理如下，带 Message Pattern 参数的重载方法
+        // The same is true for overloaded methods with Message Pattern arguments
         assertEquals("hello", MessageUtils.getLocalizedMessage("hello", "World"));
         assertEquals("您好,World", MessageUtils.getLocalizedMessage("{hello}", "World"));
 
-        // 当 message code 不存在时，返回 message 原内容
+        // If message code does not exist, return the original content of message
         assertEquals("{code-not-found}", MessageUtils.getLocalizedMessage("{code-not-found}"));
         assertEquals("code-not-found", MessageUtils.getLocalizedMessage("{microsphere-test.code-not-found}"));
         assertEquals("code-not-found", MessageUtils.getLocalizedMessage("{common.code-not-found}"));
 
-        // 测试英文
+        // The test of English
         assertEquals("hello", MessageUtils.getLocalizedMessage("hello", Locale.ENGLISH, "World"));
         assertEquals("Hello,World", MessageUtils.getLocalizedMessage("{hello}", Locale.ENGLISH, "World"));
     }
