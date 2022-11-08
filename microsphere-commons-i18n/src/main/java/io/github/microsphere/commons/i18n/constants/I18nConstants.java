69687af3104984436559f667ab82215ca8ab7ac7
Mercy Ma <mercyblitz@gmail.com>
2022-11-08 09:46:20 +0800

    Update I18nConstants.java

--- microsphere-commons-i18n/src/main/java/io/github/microsphere/commons/i18n/constants/I18nConstants.java
+++ microsphere-commons-i18n/src/main/java/io/github/microsphere/commons/i18n/constants/I18nConstants.java
@@ -5,7 +5,7 @@ import io.github.microsphere.commons.i18n.ServiceMessageSource;
 import java.util.Locale;
 
 /**
- * 国际化属性常量
+ * Internationalization property constants
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
  * @since 1.0.0
@@ -15,47 +15,47 @@ public interface I18nConstants {
     String PROPERTY_NAME_PREFIX = "microsphere.i18n.";
 
     /**
-     * 激活 配置名称
+     * Enabled Configuration Name
      */
     String ENABLED_PROPERTY_NAME = PROPERTY_NAME_PREFIX + "enabled";
 
     /**
-     * 默认激活
+     * Enabled By Default
      */
     boolean DEFAULT_ENABLED = true;
 
     /**
-     * 默认 {@link Locale} 属性名称
+     * Default {@link Locale} property name
      */
     String DEFAULT_LOCALE_PROPERTY_NAME = PROPERTY_NAME_PREFIX + "default-locale";
 
     /**
-     * 支持的 {@link Locale} 列表属性名称
+     * Supported {@link Locale} list property names
      */
     String SUPPORTED_LOCALES_PROPERTY_NAME = PROPERTY_NAME_PREFIX + "supported-locales";
 
     /**
-     * Message Code 模式前缀
+     * Message Code pattern prefix
      */
     String MESSAGE_PATTERN_PREFIX = "{";
 
     /**
-     * Message Code 模式后缀
+     * Message Code pattern suffix
      */
     String MESSAGE_PATTERN_SUFFIX = "}";
 
     /**
-     * 通用 {@link ServiceMessageSource} Bean 名称
+     * Generic {@link ServiceMessageSource} bean name
      */
     String COMMON_SERVICE_MESSAGE_SOURCE_BEAN_NAME = "commonServiceMessageSource";
 
     /**
-     * 通用 {@link ServiceMessageSource} Bean 优先级
+     * Generic {@link ServiceMessageSource} Bean Priority
      */
     int COMMON_SERVICE_MESSAGE_SOURCE_ORDER = 500;
 
     /**
-     * 主 {@link ServiceMessageSource} Bean
+     * Primary {@link ServiceMessageSource} Bean
      */
     String SERVICE_MESSAGE_SOURCE_BEAN_NAME = "serviceMessageSource";
 }
