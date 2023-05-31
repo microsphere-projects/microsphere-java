8c592d6e5e1053d4d1c19b63fa38a3789b1b2819
Mercy Ma <mercyblitz@gmail.com>
2023-05-31 11:50:28 +0800

    Refactor ThrowableConsumer

--- microsphere-core/src/main/java/io/microsphere/lang/function/ThrowableConsumer.java
+++ microsphere-core/src/main/java/io/microsphere/lang/function/ThrowableConsumer.java
@@ -16,8 +16,11 @@
  */
 package io.microsphere.lang.function;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Function;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * {@link Consumer} with {@link Throwable}
@@ -39,28 +42,68 @@ public interface ThrowableConsumer<T> {
     void accept(T t) throws Throwable;
 
     /**
-     * Executes {@link ThrowableConsumer}
+     * Executes {@link #accept(T)} with {@link #handleException(Object, Throwable) the default exception handling}
      *
      * @param t the function argument
-     * @throws RuntimeException wrappers {@link Throwable}
+     * @see #accept(T)
      */
-    default void execute(T t) throws RuntimeException {
+    default void execute(T t) {
+        execute(t, this::handleException);
+    }
+
+    /**
+     * Executes {@link #accept(T)} with the customized exception handling
+     *
+     * @param t                the function argument
+     * @param exceptionHandler the handler to handle the function argument and
+     *                         the exception that the {@link #accept(T)} method throws
+     * @throws NullPointerException if <code>exceptionHandler</code> is <code>null</code>
+     */
+    default void execute(T t, BiConsumer<T, Throwable> exceptionHandler) throws NullPointerException {
+        requireNonNull(exceptionHandler, "The exceptionHandler must not be null");
         try {
             accept(t);
         } catch (Throwable e) {
-            throw new RuntimeException(e.getMessage(), e.getCause());
+            exceptionHandler.accept(t, e);
         }
     }
 
     /**
-     * Executes {@link ThrowableConsumer}
+     * Handle any exception that the {@link #accept(T)} method throws
+     *
+     * @param t       the value to be consumed
+     * @param failure the instance of {@link Throwable}
+     */
+    default void handleException(T t, Throwable failure) {
+        throw new RuntimeException(failure);
+    }
+
+    /**
+     * Executes {@link ThrowableConsumer} with {@link #handleException(Object, Throwable) the default exception handling}
      *
      * @param t        the function argument
      * @param consumer {@link ThrowableConsumer}
      * @param <T>      the source type
      * @return the result after execution
+     * @throws NullPointerException if <code>consumer</code> is <code>null</code>
      */
-    static <T> void execute(T t, ThrowableConsumer<T> consumer) {
+    static <T> void execute(T t, ThrowableConsumer<T> consumer) throws NullPointerException {
+        requireNonNull(consumer, "The consumer must not be null");
         consumer.execute(t);
+    }
+
+    /**
+     * Executes {@link ThrowableConsumer} with the customized exception handling
+     *
+     * @param t                the function argument
+     * @param consumer         {@link ThrowableConsumer}
+     * @param exceptionHandler the handler to handle any {@link Throwable exception} that the {@link #accept(T)} ()} method throws
+     * @param <T>              the source type
+     * @return the result after execution
+     * @throws NullPointerException if <code>consumer</code> and <code>exceptionHandler</code> is <code>null</code>
+     */
+    static <T> void execute(T t, ThrowableConsumer<T> consumer, BiConsumer<T, Throwable> exceptionHandler) throws NullPointerException {
+        requireNonNull(consumer, "The consumer must not be null");
+        consumer.execute(t, exceptionHandler);
     }
 }
