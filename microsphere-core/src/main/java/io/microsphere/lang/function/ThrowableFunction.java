75b1ee764c70454648ffe9d4d39c9156717d67e9
Mercy Ma <mercyblitz@gmail.com>
2023-05-31 11:50:33 +0800

    Update ThrowableFunction.java

--- microsphere-core/src/main/java/io/microsphere/lang/function/ThrowableFunction.java
+++ microsphere-core/src/main/java/io/microsphere/lang/function/ThrowableFunction.java
@@ -16,7 +16,10 @@
  */
 package io.microsphere.lang.function;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * {@link Function} with {@link Throwable}
@@ -40,32 +43,74 @@ public interface ThrowableFunction<T, R> {
     R apply(T t) throws Throwable;
 
     /**
-     * Executes {@link ThrowableFunction}
+     * Executes {@link #apply(T)} with {@link #handleException(T, Throwable) the default exception handling}
      *
      * @param t the function argument
      * @return the function result
-     * @throws RuntimeException wrappers {@link Throwable}
      */
     default R execute(T t) throws RuntimeException {
+        return execute(t, this::handleException);
+    }
+
+    /**
+     * Executes {@link #apply(T)} with the customized exception handling
+     *
+     * @param t                the function argument
+     * @param exceptionHandler the handler to handle the function argument and
+     *                         the exception that the {@link #apply(T)} method throws
+     * @return the function result
+     */
+    default R execute(T t, BiFunction<T, Throwable, R> exceptionHandler) throws RuntimeException {
         R result = null;
         try {
             result = apply(t);
         } catch (Throwable e) {
-            throw new RuntimeException(e.getCause());
+            result = exceptionHandler.apply(t, e);
         }
         return result;
     }
 
     /**
-     * Executes {@link ThrowableFunction}
+     * Handle any exception that the {@link #apply(T)} method throws
+     *
+     * @param t       the value to be consumed
+     * @param failure the instance of {@link Throwable}
+     */
+    default R handleException(T t, Throwable failure) {
+        throw new RuntimeException(failure);
+    }
+
+    /**
+     * Executes {@link ThrowableFunction} with {@link #handleException(T, Throwable) the default exception handling}
      *
      * @param t        the function argument
      * @param function {@link ThrowableFunction}
      * @param <T>      the source type
      * @param <R>      the return type
      * @return the result after execution
+     * @throws NullPointerException if <code>function</code> is <code>null</code>
      */
-    static <T, R> R execute(T t, ThrowableFunction<T, R> function) {
+    static <T, R> R execute(T t, ThrowableFunction<T, R> function) throws NullPointerException {
+        requireNonNull(function, "The function must not be null");
         return function.execute(t);
+    }
+
+    /**
+     * Executes {@link ThrowableFunction} with the customized exception handling
+     *
+     * @param t                the function argument
+     * @param exceptionHandler the handler to handle the function argument and
+     *                         the exception that the {@link #apply(T)} method throws
+     * @param function         {@link ThrowableFunction}
+     * @param <T>              the source type
+     * @param <R>              the return type
+     * @return the result after execution
+     * @throws NullPointerException if <code>function</code> and <code>exceptionHandler</code> is <code>null</code>
+     */
+    static <T, R> R execute(T t, ThrowableFunction<T, R> function, BiFunction<T, Throwable, R> exceptionHandler)
+            throws NullPointerException {
+        requireNonNull(function, "The function must not be null");
+        requireNonNull(exceptionHandler, "The exceptionHandler must not be null");
+        return function.execute(t, exceptionHandler);
     }
 }
