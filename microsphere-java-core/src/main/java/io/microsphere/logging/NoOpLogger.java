e5a828634a3ea0172dcf51e181bcdae2bc32ebfe
小马哥（mercyblitz） <mercyblitz@gmail.com>
2024-06-21 21:15:56 +0800

    Create NoOpLogger.java

---
+++ microsphere-java-core/src/main/java/io/microsphere/logging/NoOpLogger.java
@@ -0,0 +1,116 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.microsphere.logging;
+
+/**
+ * No-Operation {@link Logger}
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * @see Logger
+ * @since 1.0.0
+ */
+final class NoOpLogger extends AbstractLogger {
+
+    NoOpLogger(String name) {
+        super(name);
+    }
+
+    @Override
+    public boolean isTraceEnabled() {
+        return false;
+    }
+
+    @Override
+    public void trace(String message) {
+    }
+
+    @Override
+    public void trace(String format, Object... arguments) {
+    }
+
+    @Override
+    public void trace(String message, Throwable t) {
+    }
+
+    @Override
+    public boolean isDebugEnabled() {
+        return false;
+    }
+
+    @Override
+    public void debug(String message) {
+    }
+
+    @Override
+    public void debug(String format, Object... arguments) {
+    }
+
+    @Override
+    public void debug(String message, Throwable t) {
+    }
+
+    @Override
+    public boolean isInfoEnabled() {
+        return false;
+    }
+
+    @Override
+    public void info(String message) {
+    }
+
+    @Override
+    public void info(String message, Object... arguments) {
+    }
+
+    @Override
+    public void info(String message, Throwable t) {
+    }
+
+    @Override
+    public boolean isWarnEnabled() {
+        return false;
+    }
+
+    @Override
+    public void warn(String message) {
+    }
+
+    @Override
+    public void warn(String format, Object... arguments) {
+    }
+
+    @Override
+    public void warn(String message, Throwable t) {
+    }
+
+    @Override
+    public boolean isErrorEnabled() {
+        return false;
+    }
+
+    @Override
+    public void error(String message) {
+    }
+
+    @Override
+    public void error(String format, Object... arguments) {
+    }
+
+    @Override
+    public void error(String message, Throwable t) {
+    }
+}
