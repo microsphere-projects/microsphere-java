682fa12d50d1490104900d79eea7971cd0bda3ff
小马哥（mercyblitz） <mercyblitz@gmail.com>
2024-06-23 23:58:31 +0800

    Update AccessibleObjectUtils.java

--- microsphere-java-core/src/main/java/io/microsphere/reflect/AccessibleObjectUtils.java
+++ microsphere-java-core/src/main/java/io/microsphere/reflect/AccessibleObjectUtils.java
@@ -19,12 +19,18 @@ package io.microsphere.reflect;
 import io.microsphere.lang.function.ThrowableConsumer;
 import io.microsphere.lang.function.ThrowableFunction;
 import io.microsphere.lang.function.ThrowableSupplier;
+import io.microsphere.logging.Logger;
 import io.microsphere.util.BaseUtils;
 
+import java.lang.invoke.MethodHandle;
 import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.Member;
 import java.lang.reflect.Method;
+
+import static io.microsphere.invoke.MethodHandleUtils.findVirtual;
+import static io.microsphere.logging.LoggerFactory.getLogger;
 
 /**
  * The utilities class of {@link AccessibleObject}
@@ -34,6 +40,19 @@ import java.lang.reflect.Method;
  * @since 1.0.0
  */
 public abstract class AccessibleObjectUtils extends BaseUtils {
+
+    private static final Logger logger = getLogger(AccessibleObjectUtils.class);
+
+    /**
+     * The method name of {@link AccessibleObject#canAccess(Object)} since Java 9
+     */
+    private static final String canAccessMethodName = "canAccess";
+
+    /**
+     * The {@link MethodHandle} of {@link AccessibleObject#canAccess(Object)} since Java 9
+     * if <code>canAccessMethodHandle == null</code>, it indicates the version of Java is less than 9
+     */
+    private static final MethodHandle canAccessMethodHandle = findVirtual(AccessibleObject.class, canAccessMethodName, Object.class);
 
     /**
      * Execute an {@link AccessibleObject} instance
@@ -84,5 +103,36 @@ public abstract class AccessibleObjectUtils extends BaseUtils {
             }
         }
         return result;
+    }
+
+    /**
+     * Test if the caller can access this reflected object. If this reflected
+     * object corresponds to an instance method or field then this method tests
+     * if the caller can access the given {@code obj} with the reflected object.
+     * For instance methods or fields then the {@code obj} argument must be an
+     * instance of the {@link Member#getDeclaringClass() declaring class}. For
+     * static members and constructors then {@code obj} must be {@code null}.
+     *
+     * @param object an instance object of the declaring class of this reflected object if it is an instance method or field
+     * @return {@code true} if the caller can access this reflected object.
+     */
+    public static boolean canAccess(Object object, AccessibleObject accessibleObject) {
+
+        Boolean access = tryCanAccess(object, accessibleObject);
+
+        return access == null ? accessibleObject.isAccessible() : access;
+    }
+
+    private static Boolean tryCanAccess(Object object, AccessibleObject accessibleObject) {
+        Boolean access = null;
+        if (canAccessMethodHandle != null) { // Java 9+
+            try {
+                access = (boolean) canAccessMethodHandle.invokeExact(accessibleObject, object);
+            } catch (Throwable e) {
+                logger.error("java.lang.reflect.AccessibleObject#canAccess(Object) can't be invoked, object : {} , accessible object : {}",
+                        object, accessibleObject, e);
+            }
+        }
+        return access;
     }
 }
