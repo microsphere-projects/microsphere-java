8eeb7ac15c034da2616db64f2cf895764d3b91ed
Mercy Ma <mercyblitz@gmail.com>
2024-12-05 10:33:14 +0800

    Update ExperimentalTest.java

--- microsphere-java-core/src/test/java/io/microsphere/annotation/ExperimentalTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/annotation/ExperimentalTest.java
@@ -18,7 +18,6 @@ package io.microsphere.annotation;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 /**
