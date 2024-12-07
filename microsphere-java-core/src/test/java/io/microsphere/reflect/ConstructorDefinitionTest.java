898fdb8e081f684d00eace306660df21e4adaea9
Mercy Ma <mercyblitz@gmail.com>
2024-12-07 19:20:41 +0800

    Update ConstructorDefinitionTest.java

--- microsphere-java-core/src/test/java/io/microsphere/reflect/ConstructorDefinitionTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/reflect/ConstructorDefinitionTest.java
@@ -1,9 +1,14 @@
 package io.microsphere.reflect;
 
+import io.microsphere.lang.Deprecation;
+import io.microsphere.util.ClassUtils;
 import io.microsphere.util.Version;
 import org.junit.jupiter.api.Test;
 
+import static io.microsphere.lang.DeprecationTest.DEPRECATION;
 import static io.microsphere.lang.DeprecationTest.SINCE;
+import static io.microsphere.util.ClassUtils.getTypeName;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -16,18 +21,53 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
  */
 public class ConstructorDefinitionTest {
 
-    private static final String CLASS_NAME = "io.microsphere.reflect.ConstructorDefinitionTest";
+    static class TestData {
+
+        private final String name;
+
+        public TestData() {
+            this(null);
+        }
+
+        public TestData(String name) {
+            this.name = name;
+        }
+    }
 
     @Test
     public void test() {
-        ConstructorDefinition cd = new ConstructorDefinition(SINCE, CLASS_NAME);
+        assertConstructorDefinition(SINCE, DEPRECATION);
+        assertConstructorDefinition(SINCE, DEPRECATION, "test");
 
-        assertEquals(Version.of(SINCE), cd.getSince());
-        assertEquals(CLASS_NAME, cd.getDeclaredClassName());
+        assertConstructorDefinition(SINCE);
+        assertConstructorDefinition(SINCE, "test");
+    }
+
+    private void assertConstructorDefinition(String since, Object... args) {
+        assertConstructorDefinition(since, null, args);
+    }
+
+    private void assertConstructorDefinition(String since, Deprecation deprecation, Object... args) {
+        String className = getTypeName(TestData.class);
+        String[] parameterClassNames = new String[args.length];
+        for (int i = 0; i < args.length; i++) {
+            parameterClassNames[i] = getTypeName(args[i].getClass());
+        }
+        ConstructorDefinition cd = new ConstructorDefinition(since, deprecation, className, parameterClassNames);
+
+        assertEquals(Version.of(since), cd.getSince());
+        assertEquals(className, cd.getDeclaredClassName());
+        assertArrayEquals(parameterClassNames, cd.getParameterClassNames());
+        assertEquals(cd, new ConstructorDefinition(since, deprecation, className, parameterClassNames));
+        assertNotNull(cd.toString());
+        assertNotNull(cd.hashCode());
         assertNotNull(cd.getConstructor());
+        assertNotNull(cd.getDeclaredClass());
+        assertNotNull(cd.getParameterTypes());
 
-        ConstructorDefinitionTest test = cd.newInstance();
-        assertNotNull(test);
+        TestData testData = cd.newInstance(args);
+        assertNotNull(testData);
+        assertEquals(args.length == 0 ? null : args[0], testData.name);
     }
 
 }
