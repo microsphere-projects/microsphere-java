788515baa58318f3376c7846344ff75263729bf2
Mercy Ma <mercyblitz@gmail.com>
2025-03-02 11:32:34 +0800

    Update PackageNameClassFilterTest.java

--- microsphere-java-core/src/test/java/io/microsphere/filter/PackageNameClassFilterTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/filter/PackageNameClassFilterTest.java
@@ -2,7 +2,8 @@ package io.microsphere.filter;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * {@link PackageNameClassFilter} Test
