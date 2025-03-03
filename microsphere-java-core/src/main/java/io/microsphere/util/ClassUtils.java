d312e9e37c2118fba346c956849d5e4be3e9e8c2
Mercy Ma <mercyblitz@gmail.com>
2025-03-03 11:42:02 +0800

    Update ClassUtils.java

--- microsphere-java-core/src/main/java/io/microsphere/util/ClassUtils.java
+++ microsphere-java-core/src/main/java/io/microsphere/util/ClassUtils.java
@@ -152,11 +152,6 @@ public abstract class ClassUtils extends BaseUtils {
     );
 
     /**
-     * Prefix for internal array class names: "[L"
-     */
-    private static final String INTERNAL_ARRAY_PREFIX = "[L";
-
-    /**
      * A map with primitive type name as key and corresponding primitive type as
      * value, for example: "int" -> "int.class".
      */
