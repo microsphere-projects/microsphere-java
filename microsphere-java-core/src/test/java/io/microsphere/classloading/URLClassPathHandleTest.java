da6a0aadf0c9fa3d130ea710d527428cdad0acfb
Mercy Ma <mercyblitz@gmail.com>
2025-03-03 19:52:32 +0800

    Create URLClassPathHandleTest.java

---
+++ microsphere-java-core/src/test/java/io/microsphere/classloading/URLClassPathHandleTest.java
@@ -0,0 +1,46 @@
+package io.microsphere.classloading;
+
+import org.junit.jupiter.api.Test;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import static io.microsphere.lang.Prioritized.MIN_PRIORITY;
+import static io.microsphere.net.URLUtils.EMPTY_URL_ARRAY;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+/**
+ * {@link URLClassPathHandle} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * @see URLClassPathHandle
+ * @since 1.0.0
+ */
+public class URLClassPathHandleTest {
+
+    @Test
+    public void test() {
+        URLClassPathHandle handle = new URLClassPathHandle() {
+            @Override
+            public boolean supports() {
+                return false;
+            }
+
+            @Override
+            public boolean removeURL(ClassLoader classLoader, URL url) {
+                return false;
+            }
+        };
+
+        assertFalse(handle.supports());
+        assertSame(EMPTY_URL_ARRAY, handle.getURLs(null));
+        URL[] urls = new URL[0];
+        assertArrayEquals(urls, handle.getURLs(new URLClassLoader(urls)));
+        assertFalse(handle.removeURL(null, null));
+        assertEquals(MIN_PRIORITY, handle.getPriority());
+    }
+
+}
