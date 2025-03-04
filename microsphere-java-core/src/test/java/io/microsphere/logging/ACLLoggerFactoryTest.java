c1e49dc6777b85db042a6ab52ad8deb7ed9fc63e
Mercy Ma <mercyblitz@gmail.com>
2025-03-04 18:27:45 +0800

    Create ACLLoggerFactoryTest.java

---
+++ microsphere-java-core/src/test/java/io/microsphere/logging/ACLLoggerFactoryTest.java
@@ -0,0 +1,17 @@
+package io.microsphere.logging;
+
+/**
+ * {@link ACLLoggerFactory} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * @see ACLLoggerFactory
+ * @since 1.0.0
+ */
+public class ACLLoggerFactoryTest extends AbstractLoggerTest {
+
+    @Override
+    protected Logger createLogger() {
+        ACLLoggerFactory factory = new ACLLoggerFactory();
+        return factory.createLogger("acl");
+    }
+}
