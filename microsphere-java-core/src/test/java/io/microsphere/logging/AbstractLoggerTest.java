eab590aae5a45394aaf955c91246d1c5a7538f97
Mercy Ma <mercyblitz@gmail.com>
2025-03-04 17:48:37 +0800

    Update AbstractLoggerTest.java

--- microsphere-java-core/src/test/java/io/microsphere/logging/AbstractLoggerTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/logging/AbstractLoggerTest.java
@@ -19,7 +19,6 @@ package io.microsphere.logging;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
