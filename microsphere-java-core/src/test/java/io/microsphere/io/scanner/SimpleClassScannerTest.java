cb8b66d8abc334eec980bf3d3a34492f38452c63
Mercy Ma <mercyblitz@gmail.com>
2025-03-04 10:21:15 +0800

    Update SimpleClassScannerTest.java

--- microsphere-java-core/src/test/java/io/microsphere/io/scanner/SimpleClassScannerTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/io/scanner/SimpleClassScannerTest.java
@@ -4,12 +4,18 @@
 package io.microsphere.io.scanner;
 
 import io.microsphere.AbstractTestCase;
+import io.microsphere.util.ClassLoaderUtils;
 import org.junit.jupiter.api.Test;
 
+import javax.annotation.Nonnull;
+import java.net.URL;
 import java.util.Set;
 
+import static io.microsphere.util.ClassLoaderUtils.getClassResource;
+import static io.microsphere.util.ClassLoaderUtils.getDefaultClassLoader;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * {@link SimpleClassScannerTest}
@@ -24,11 +30,22 @@ public class SimpleClassScannerTest extends AbstractTestCase {
     private SimpleClassScanner simpleClassScanner = SimpleClassScanner.INSTANCE;
 
     @Test
-    public void testScan() {
+    public void testScanInPackage() {
         Set<Class<?>> classesSet = simpleClassScanner.scan(classLoader, "io.microsphere.io.scanner");
         assertFalse(classesSet.isEmpty());
 
         classesSet = simpleClassScanner.scan(classLoader, "javax.annotation.concurrent", false, true);
         assertEquals(4, classesSet.size());
+
+        classesSet = simpleClassScanner.scan(classLoader, "i", false, true);
+        assertTrue(classesSet.isEmpty());
     }
+
+    @Test
+    public void testScanInArchive() {
+        URL nonnullClassResource = getClassResource(classLoader, Nonnull.class);
+        Set<Class<?>> classesSet = simpleClassScanner.scan(classLoader, nonnullClassResource, false);
+        assertFalse(classesSet.isEmpty());
+    }
+
 }
