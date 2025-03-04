4391a0e4a132fb87fd3f65551a5472d35bd11916
Mercy Ma <mercyblitz@gmail.com>
2025-03-04 10:23:03 +0800

    Update BeanPropertyTest.java

--- microsphere-java-core/src/test/java/io/microsphere/beans/BeanPropertyTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/beans/BeanPropertyTest.java
@@ -19,9 +19,6 @@ package io.microsphere.beans;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
