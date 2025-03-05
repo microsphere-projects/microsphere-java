edaa099fd545d03fa06f43398e551de0e5fa2c3c
Mercy Ma <mercyblitz@gmail.com>
2025-03-06 02:56:18 +0800

    Create IOUtilsTest.java

---
+++ microsphere-java-core/src/test/java/io/microsphere/io/IOUtilsTest.java
@@ -0,0 +1,47 @@
+package io.microsphere.io;
+
+import org.junit.jupiter.api.Test;
+
+import static io.microsphere.io.IOUtils.BUFFER_SIZE;
+import static io.microsphere.io.IOUtils.DEFAULT_BUFFER_SIZE;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * {@link IOUtils} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * @see IOUtils
+ * @since 1.0.0
+ */
+public class IOUtilsTest {
+
+    @Test
+    public void testConstants() {
+        assertEquals(4096, DEFAULT_BUFFER_SIZE);
+        assertEquals(4096, BUFFER_SIZE);
+    }
+
+    @Test
+    public void testToByteArray() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+
+    @Test
+    public void testToString1() {
+    }
+
+    @Test
+    public void testCopy() {
+    }
+
+    @Test
+    public void testClose() {
+    }
+
+    @Test
+    public void testClose0() {
+    }
+}
