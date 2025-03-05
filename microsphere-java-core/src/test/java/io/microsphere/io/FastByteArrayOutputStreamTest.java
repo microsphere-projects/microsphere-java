fa46cd651c7af96b2164fc80068ccbfb6131c62a
Mercy Ma <mercyblitz@gmail.com>
2025-03-06 02:56:59 +0800

    Update FastByteArrayOutputStreamTest.java

--- microsphere-java-core/src/test/java/io/microsphere/io/FastByteArrayOutputStreamTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/io/FastByteArrayOutputStreamTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * {@link FastByteArrayOutputStream} Test
