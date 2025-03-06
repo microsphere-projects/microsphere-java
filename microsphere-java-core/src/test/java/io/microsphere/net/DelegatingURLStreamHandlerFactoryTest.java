05f2535f6ab044a9110cb75b8ff44d451b73523a
Mercy Ma <mercyblitz@gmail.com>
2025-03-06 13:55:01 +0800

    Create DelegatingURLStreamHandlerFactoryTest.java

---
+++ microsphere-java-core/src/test/java/io/microsphere/net/DelegatingURLStreamHandlerFactoryTest.java
@@ -0,0 +1,37 @@
+package io.microsphere.net;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+/**
+ * {@link DelegatingURLStreamHandlerFactory Test}
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * @see DelegatingURLStreamHandlerFactory
+ * @since 1.0.0
+ */
+public class DelegatingURLStreamHandlerFactoryTest {
+
+    private StandardURLStreamHandlerFactory delegate;
+
+    private DelegatingURLStreamHandlerFactory factory;
+
+    @BeforeEach
+    public void init() {
+        this.delegate = new StandardURLStreamHandlerFactory();
+        this.factory = new DelegatingURLStreamHandlerFactory(this.delegate);
+    }
+
+    @Test
+    public void testCreateURLStreamHandler() {
+        assertNotNull(this.factory.createURLStreamHandler("file"));
+    }
+
+    @Test
+    public void testGetDelegate() {
+        assertSame(this.delegate, this.factory.getDelegate());
+    }
+}
