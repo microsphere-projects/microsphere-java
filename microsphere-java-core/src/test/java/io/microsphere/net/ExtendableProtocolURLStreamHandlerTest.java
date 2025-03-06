760377097e7fb5eafb032695287b3af3d2d3bc3e
Mercy Ma <mercyblitz@gmail.com>
2025-03-06 19:39:17 +0800

    Update ExtendableProtocolURLStreamHandlerTest.java

--- microsphere-java-core/src/test/java/io/microsphere/net/ExtendableProtocolURLStreamHandlerTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/net/ExtendableProtocolURLStreamHandlerTest.java
@@ -32,9 +32,13 @@ import static io.microsphere.collection.ListUtils.newLinkedList;
 import static io.microsphere.collection.ListUtils.ofList;
 import static io.microsphere.collection.SetUtils.ofSet;
 import static io.microsphere.net.ExtendableProtocolURLStreamHandler.appendHandlePackage;
+import static io.microsphere.net.ExtendableProtocolURLStreamHandler.assertClassName;
+import static io.microsphere.net.ExtendableProtocolURLStreamHandler.assertClassTopLevel;
+import static io.microsphere.net.ExtendableProtocolURLStreamHandler.assertPackage;
 import static io.microsphere.net.ExtendableProtocolURLStreamHandler.getHandlePackages;
 import static io.microsphere.net.ExtendableProtocolURLStreamHandler.getHandlePackagesPropertyValue;
 import static io.microsphere.net.URLUtils.toExternalForm;
+import static io.microsphere.util.ClassLoaderUtils.resolveClass;
 import static java.net.Proxy.NO_PROXY;
 import static java.util.Collections.emptyList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -74,6 +78,38 @@ public class ExtendableProtocolURLStreamHandlerTest {
     public void destroy() {
     }
 
+    @Test
+    public void testAssertClassTopLevelOnLocalClass() {
+        class LocalClass extends ExtendableProtocolURLStreamHandler {
+
+        }
+        assertThrows(IllegalArgumentException.class, () -> assertClassTopLevel(LocalClass.class));
+    }
+
+    @Test
+    public void testAssertClassTopLevelOnMemberClass() {
+        assertThrows(IllegalArgumentException.class, () -> assertClassTopLevel(MemberClass.class));
+    }
+
+    @Test
+    public void testAssertClassTopLevelOnAnonymousClass() {
+        assertThrows(IllegalArgumentException.class, () ->
+                assertClassTopLevel(new ExtendableProtocolURLStreamHandler() {
+                }.getClass()));
+    }
+
+    @Test
+    public void testAssertClassNameOnWrongClassName() {
+        assertThrows(IllegalArgumentException.class, () -> assertClassName(MemberClass.class));
+    }
+
+    @Test
+    public void testAssertPackageOnSunHandlerClass() {
+        Class<?> handler = resolveClass("sun.net.www.protocol.file.Handler");
+        if (handler != null) {
+            assertThrows(IllegalArgumentException.class, () -> assertPackage(handler));
+        }
+    }
 
     @Test
     public void testConstructorWithProtocolArg() {
@@ -252,5 +288,8 @@ public class ExtendableProtocolURLStreamHandlerTest {
     @Test
     public void testToString() {
         assertEquals("io.microsphere.net.console.Handler {defaultPort = -1 , protocol = 'console'}", handler.toString());
+    }
+
+    private static class MemberClass extends ExtendableProtocolURLStreamHandler {
     }
 }
