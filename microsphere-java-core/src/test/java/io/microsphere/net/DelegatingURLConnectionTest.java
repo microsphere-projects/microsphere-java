3fe31de497aba8e2d6cd8867191bc0fd8c12a740
Mercy Ma <mercyblitz@gmail.com>
2025-03-06 17:12:30 +0800

    Update DelegatingURLConnectionTest.java

--- microsphere-java-core/src/test/java/io/microsphere/net/DelegatingURLConnectionTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/net/DelegatingURLConnectionTest.java
@@ -143,7 +143,7 @@ public class DelegatingURLConnectionTest {
 
     @Test
     public void testGetHeaderFields() {
-        assertTrue(urlConnection.getHeaderFields().isEmpty());
+        assertNotNull(urlConnection.getHeaderFields());
     }
 
     @Test
