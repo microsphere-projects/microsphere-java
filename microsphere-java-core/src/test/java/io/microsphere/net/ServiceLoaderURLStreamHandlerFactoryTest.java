76fb97407b0d00a74f0450656e9437aa0c01fd65
Mercy Ma <mercyblitz@gmail.com>
2025-03-07 13:39:05 +0800

    Update ServiceLoaderURLStreamHandlerFactoryTest.java

--- microsphere-java-core/src/test/java/io/microsphere/net/ServiceLoaderURLStreamHandlerFactoryTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/net/ServiceLoaderURLStreamHandlerFactoryTest.java
@@ -17,9 +17,7 @@
 package io.microsphere.net;
 
 import io.microsphere.io.IOUtils;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
