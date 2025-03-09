a7f0ac5b344d8e93dda54c6826dcac0cb6a92cec
Mercy Ma <mercyblitz@gmail.com>
2025-03-09 20:31:36 +0800

    Update ByteArrayToObjectConverterTest.java

--- microsphere-java-core/src/test/java/io/microsphere/io/ByteArrayToObjectConverterTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/io/ByteArrayToObjectConverterTest.java
@@ -23,7 +23,6 @@ import org.junit.jupiter.api.Test;
 import static io.microsphere.convert.ObjectToByteArrayConverter.INSTANCE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
 /**
