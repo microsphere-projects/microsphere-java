ad85b6d888f4f12235dee51e481119f8806f94d2
Mercy Ma <mercyblitz@gmail.com>
2025-03-11 03:32:59 +0800

    Update ConstructorUtilsTest.java

--- microsphere-java-core/src/test/java/io/microsphere/reflect/ConstructorUtilsTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/reflect/ConstructorUtilsTest.java
@@ -13,7 +13,6 @@ import static io.microsphere.reflect.ConstructorUtils.newInstance;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
