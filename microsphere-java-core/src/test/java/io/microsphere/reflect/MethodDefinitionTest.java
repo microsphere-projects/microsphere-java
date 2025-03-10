4d30d715e17b9291daff4e9b084e58a7ab886a5b
Mercy Ma <mercyblitz@gmail.com>
2025-03-11 04:02:38 +0800

    Update MethodDefinitionTest.java

--- microsphere-java-core/src/test/java/io/microsphere/reflect/MethodDefinitionTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/reflect/MethodDefinitionTest.java
@@ -21,6 +21,9 @@ import io.microsphere.logging.LoggerFactory;
 import io.microsphere.util.Version;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static io.microsphere.collection.Lists.ofList;
 import static io.microsphere.util.ArrayUtils.ofArray;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -35,28 +38,36 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * @see MethodDefinition
  * @since 1.0.0
  */
-public class MethodDefinitionTest {
+public class MethodDefinitionTest extends AbstractExecutableDefinitionTest<MethodDefinition> {
 
     private static final Logger logger = LoggerFactory.getLogger(MethodDefinitionTest.class);
 
+    @Override
+    protected List<Object> getTailConstructorArguments() {
+        return ofList("log", ofArray("java.lang.String"));
+    }
+
     @Test
-    public void test() {
-        MethodDefinition md = new MethodDefinition("1.0.0", "io.microsphere.reflect.MethodDefinitionTest", "log", "java.lang.String");
-
-        assertEquals(Version.of("1.0.0"), md.getSince());
-        assertNull(md.getDeprecation());
-        assertEquals(MethodDefinitionTest.class, md.getResolvedClass());
-        assertEquals("log", md.getMethodName());
-        assertArrayEquals(ofArray(String.class), md.getParameterTypes());
-        assertTrue(md.isPresent());
-        assertNotNull(md.getMethod());
-
-        assertNull(md.invoke(this, "test"));
+    public void testGetMethodName() {
+        for (MethodDefinition definition : definitions) {
+            assertEquals(definition.getName(), definition.getMethodName());
+        }
     }
 
-    private void assertObjectMethods(MethodDefinition md) {
-        MethodDefinition copy = new MethodDefinition(md.since, md.deprecation, md.getDeclaredClassName(), md.name, md.parameterClassNames);
+    @Test
+    public void testGetMethod() {
+        for (MethodDefinition definition : definitions) {
+            assertEquals(definition.getMember(), definition.getMethod());
+        }
     }
+
+    @Test
+    public void testInvoke() {
+        for (MethodDefinition definition : definitions) {
+            assertNull(definition.invoke(this, definition.toString()));
+        }
+    }
+
 
     private void log(String message) {
         if (logger.isDebugEnabled()) {
