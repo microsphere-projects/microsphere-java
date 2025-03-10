55bf4f2c87d3dda9f51f0475a733347fc9c02713
Mercy Ma <mercyblitz@gmail.com>
2025-03-10 13:05:45 +0800

    Update ManagementUtilsTest.java

--- microsphere-java-core/src/test/java/io/microsphere/management/ManagementUtilsTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/management/ManagementUtilsTest.java
@@ -3,7 +3,19 @@ package io.microsphere.management;
 import io.microsphere.AbstractTestCase;
 import org.junit.jupiter.api.Test;
 
+import static io.microsphere.management.JmxUtils.getRuntimeMXBean;
+import static io.microsphere.management.ManagementUtils.GET_PROCESS_ID_METHOD_NAME;
+import static io.microsphere.management.ManagementUtils.JVM_FIELD_NAME;
+import static io.microsphere.management.ManagementUtils.UNKNOWN_PROCESS_ID;
+import static io.microsphere.management.ManagementUtils.getCurrentProcessId;
+import static io.microsphere.management.ManagementUtils.getNativeCurrentPID;
+import static io.microsphere.management.ManagementUtils.jvm;
+import static io.microsphere.management.ManagementUtils.resolveCurrentPID;
+import static io.microsphere.management.ManagementUtils.runtimeMXBean;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
@@ -18,12 +30,27 @@ public class ManagementUtilsTest extends AbstractTestCase {
 
     @Test
     public void testStaticFields() {
-        assertNotNull(ManagementUtils.jvm);
+        assertEquals(-1, UNKNOWN_PROCESS_ID);
+        assertEquals("jvm", JVM_FIELD_NAME);
+        assertEquals("getProcessId", GET_PROCESS_ID_METHOD_NAME);
+        assertSame(getRuntimeMXBean(), runtimeMXBean);
+        assertNotNull(jvm);
+    }
+
+    @Test
+    public void testGetNativeCurrentPID() {
+        assertNotNull(getNativeCurrentPID());
+    }
+
+    @Test
+    public void testResolveCurrentPID() {
+        assertNotEquals(-1, resolveCurrentPID());
     }
 
     @Test
     public void testGetCurrentProcessId() {
-        int currentProcessId = ManagementUtils.getCurrentProcessId();
+        int currentProcessId = getCurrentProcessId();
         assertTrue(currentProcessId > 0);
     }
+
 }
