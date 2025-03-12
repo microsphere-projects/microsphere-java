9bffb2e92bd9b7b74e7d191ead2994a5113ea3f8
Mercy Ma <mercyblitz@gmail.com>
2025-03-12 17:27:18 +0800

    Update Streams.java

--- microsphere-java-core/src/main/java/io/microsphere/lang/function/Streams.java
+++ microsphere-java-core/src/main/java/io/microsphere/lang/function/Streams.java
@@ -35,8 +35,17 @@ import static java.util.stream.Collectors.toList;
  */
 public interface Streams {
 
+    static <T> Stream<T> stream(T... values) {
+        return Stream.of(values);
+    }
+
     static <T> Stream<T> stream(Iterable<T> iterable) {
         return StreamSupport.stream(iterable.spliterator(), false);
+    }
+
+    static <T, S extends Iterable<T>> Stream<T> filterStream(T[] values, Predicate<? super T> predicate) {
+        Stream<T> stream = stream(values);
+        return stream.filter(predicate);
     }
 
     static <T, S extends Iterable<T>> Stream<T> filterStream(S values, Predicate<? super T> predicate) {
