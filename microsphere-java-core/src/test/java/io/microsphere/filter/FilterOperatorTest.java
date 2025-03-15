7a50347bcf00f9f1e4709ca0c3bc1e8054c6a164
Mercy Ma <mercyblitz@gmail.com>
2025-03-15 16:58:41 +0800

    Update FilterOperatorTest.java

--- microsphere-java-core/src/test/java/io/microsphere/filter/FilterOperatorTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/filter/FilterOperatorTest.java
@@ -23,7 +23,7 @@ public class FilterOperatorTest {
     PackageNameClassFilter filter2 = new PackageNameClassFilter("io.microsphere", true);
 
     @Test
-    public void testAnd() {
+    public void testAND() {
         Filter filter = AND.createFilter(filter1, filter2);
         assertFalse(filter.accept(null));
 
@@ -32,7 +32,19 @@ public class FilterOperatorTest {
     }
 
     @Test
-    public void testOr() {
+    public void testANDOnNullFilters() {
+        Filter filter = AND.createFilter(null);
+        assertTrue(filter.accept(null));
+    }
+
+    @Test
+    public void testANDOnEmptyFilters() {
+        Filter filter = AND.createFilter(new Filter[0]);
+        assertTrue(filter.accept(null));
+    }
+
+    @Test
+    public void testOR() {
         Filter filter = OR.createFilter(filter1, filter2);
         assertTrue(filter.accept(null));
 
@@ -41,13 +53,36 @@ public class FilterOperatorTest {
     }
 
     @Test
-    public void testXor() {
+    public void testOROnNullFilters() {
+        Filter filter = OR.createFilter(null);
+        assertTrue(filter.accept(null));
+    }
+
+    @Test
+    public void testOROnEmptyFilters() {
+        Filter filter = OR.createFilter(new Filter[0]);
+        assertTrue(filter.accept(null));
+    }
+
+    @Test
+    public void testXOR() {
         Filter filter = XOR.createFilter(filter1, filter2);
         assertFalse(filter.accept(null));
 
         filter = XOR.createFilter(filter2, filter2);
         assertTrue(filter.accept(null));
     }
+    
+    @Test
+    public void testXOROnNullFilters() {
+        Filter filter = XOR.createFilter(null);
+        assertTrue(filter.accept(null));
+    }
 
+    @Test
+    public void testXOROnEmptyFilters() {
+        Filter filter = XOR.createFilter(new Filter[0]);
+        assertTrue(filter.accept(null));
+    }
 
 }
