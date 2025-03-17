7b788be50dc29cebaea4f5767e88074800ab3ace
Mercy Ma <mercyblitz@gmail.com>
2025-03-17 14:13:58 +0800

    Update BannedArtifactClassLoadingExecutor.java

--- microsphere-java-core/src/main/java/io/microsphere/classloading/BannedArtifactClassLoadingExecutor.java
+++ microsphere-java-core/src/main/java/io/microsphere/classloading/BannedArtifactClassLoadingExecutor.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 import static io.microsphere.constants.SymbolConstants.COLON;
 import static io.microsphere.logging.LoggerFactory.getLogger;
+import static io.microsphere.util.ClassLoaderUtils.getDefaultClassLoader;
 import static io.microsphere.util.ClassLoaderUtils.removeClassPathURL;
 import static io.microsphere.util.StringUtils.isBlank;
 import static io.microsphere.util.StringUtils.split;
@@ -49,7 +50,7 @@ public class BannedArtifactClassLoadingExecutor {
     private final ArtifactDetector artifactDetector;
 
     public BannedArtifactClassLoadingExecutor() {
-        this(Thread.currentThread().getContextClassLoader());
+        this(getDefaultClassLoader());
     }
 
     public BannedArtifactClassLoadingExecutor(ClassLoader classLoader) {
