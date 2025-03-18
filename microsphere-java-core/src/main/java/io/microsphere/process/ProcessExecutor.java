b514bd194e53ab9552688e1cacc87f51c7313179
Mercy Ma <mercyblitz@gmail.com>
2025-03-18 19:54:14 +0800

    Update ProcessExecutor.java

--- microsphere-java-core/src/main/java/io/microsphere/process/ProcessExecutor.java
+++ microsphere-java-core/src/main/java/io/microsphere/process/ProcessExecutor.java
@@ -14,7 +14,6 @@ import static java.lang.Runtime.getRuntime;
  * {@link Process} Executor
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
- * @version 1.0.0
  * @see ProcessExecutor
  * @since 1.0.0
  */
