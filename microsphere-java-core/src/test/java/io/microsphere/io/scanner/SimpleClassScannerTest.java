093722a66b640647ac8f15d3b43ade739131350c
Mercy Ma <mercyblitz@gmail.com>
2025-03-18 19:53:23 +0800

    Update SimpleClassScannerTest.java

--- microsphere-java-core/src/test/java/io/microsphere/io/scanner/SimpleClassScannerTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/io/scanner/SimpleClassScannerTest.java
@@ -20,7 +20,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * {@link SimpleClassScannerTest}
  *
  * @author <a href="mercyblitz@gmail.com">Mercy<a/>
- * @version 1.0.0
  * @see SimpleClassScannerTest
  * @since 1.0.0
  */
