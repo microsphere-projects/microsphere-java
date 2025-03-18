4fd9ff52a871ba26a81fbc94f1fbe55e3d6d72c7
Mercy Ma <mercyblitz@gmail.com>
2025-03-18 19:52:38 +0800

    Update FileUtils.java

--- microsphere-java-core/src/main/java/io/microsphere/io/FileUtils.java
+++ microsphere-java-core/src/main/java/io/microsphere/io/FileUtils.java
@@ -20,7 +20,6 @@ import static io.microsphere.util.SystemUtils.IS_OS_WINDOWS;
  * {@link File} Utility
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
- * @version 1.0.0
  * @see FileUtils
  * @since 1.0.0
  */
@@ -33,7 +32,6 @@ public abstract class FileUtils extends BaseUtils {
      * @param targetFile      Target File
      * @return If <code>targetFile</code> is a sub-file of <code>parentDirectory</code> , resolve relative path, or
      * <code>null</code>
-     * @since 1.0.0
      */
     public static String resolveRelativePath(File parentDirectory, File targetFile) {
         String parentDirectoryPath = parentDirectory.getAbsolutePath();
