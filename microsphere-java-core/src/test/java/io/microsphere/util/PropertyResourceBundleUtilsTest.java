c2a009b1c433ee8e3a2579e44d61d249a553b03f
Mercy Ma <mercyblitz@gmail.com>
2025-03-19 10:10:19 +0800

    Update PropertyResourceBundleUtilsTest.java

--- microsphere-java-core/src/test/java/io/microsphere/util/PropertyResourceBundleUtilsTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/util/PropertyResourceBundleUtilsTest.java
@@ -5,9 +5,14 @@ package io.microsphere.util;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Locale;
 import java.util.ResourceBundle;
 
+import static io.microsphere.util.ClassLoaderUtils.getDefaultClassLoader;
+import static io.microsphere.util.PropertyResourceBundleUtils.DEFAULT_ENCODING;
+import static io.microsphere.util.PropertyResourceBundleUtils.DEFAULT_ENCODING_PROPERTY_NAME;
 import static io.microsphere.util.PropertyResourceBundleUtils.getBundle;
+import static io.microsphere.util.SystemUtils.FILE_ENCODING;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
@@ -18,6 +23,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * @since 1.0.0
  */
 public class PropertyResourceBundleUtilsTest {
+
+    private static final String BASE_NAME = "META-INF.test";
+
+    private static final Locale TEST_LOCALE = Locale.ROOT;
+
+    private static final String TEST_ENCODING = DEFAULT_ENCODING;
+
+    private static final ClassLoader TEST_CLASS_LOADER = getDefaultClassLoader();
+
+    @Test
+    public void testConstants() {
+        assertEquals("java.util.PropertyResourceBundle.encoding", DEFAULT_ENCODING_PROPERTY_NAME);
+        assertEquals(FILE_ENCODING, DEFAULT_ENCODING);
+    }
 
     @Test
     public void testGetBundle() {
