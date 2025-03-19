85216feb6d8b975803883374fe26556f5e0aab36
Mercy Ma <mercyblitz@gmail.com>
2025-03-19 09:10:09 +0800

    Update PropertyResourceBundleUtilsTest.java

--- microsphere-java-core/src/test/java/io/microsphere/util/PropertyResourceBundleUtilsTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/util/PropertyResourceBundleUtilsTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ResourceBundle;
 
+import static io.microsphere.util.PropertyResourceBundleUtils.getBundle;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
@@ -20,7 +21,7 @@ public class PropertyResourceBundleUtilsTest {
 
     @Test
     public void testGetBundle() {
-        ResourceBundle resourceBundle = PropertyResourceBundleUtils.getBundle("META-INF.test", "UTF-8");
+        ResourceBundle resourceBundle = getBundle("META-INF.test", "UTF-8");
         String expected = "测试名称";
         String value = resourceBundle.getString("name");
         assertEquals(expected, value);
