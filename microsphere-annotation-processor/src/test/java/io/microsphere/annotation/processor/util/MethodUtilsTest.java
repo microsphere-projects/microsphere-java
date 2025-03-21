c77e6ef8a38e5dfa920a2ad15312d9746e5d899d
Mercy Ma <mercyblitz@gmail.com>
2025-03-21 08:31:50 +0800

    Update MethodUtilsTest.java

--- microsphere-annotation-processor/src/test/java/io/microsphere/annotation/processor/util/MethodUtilsTest.java
+++ microsphere-annotation-processor/src/test/java/io/microsphere/annotation/processor/util/MethodUtilsTest.java
@@ -44,6 +44,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 /**
  * {@link MethodUtils} Test
  *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
  * @since 1.0.0
  */
 public class MethodUtilsTest extends AbstractAnnotationProcessingTest {
