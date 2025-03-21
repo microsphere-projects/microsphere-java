ff78edea1fb73a2562094cf5d1e4f49f2c8260f8
Mercy Ma <mercyblitz@gmail.com>
2025-03-21 08:31:42 +0800

    Update FieldUtilsTest.java

--- microsphere-annotation-processor/src/test/java/io/microsphere/annotation/processor/util/FieldUtilsTest.java
+++ microsphere-annotation-processor/src/test/java/io/microsphere/annotation/processor/util/FieldUtilsTest.java
@@ -55,6 +55,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 /**
  * {@link FieldUtils} Test
  *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
  * @since 1.0.0
  */
 public class FieldUtilsTest extends AbstractAnnotationProcessingTest {
