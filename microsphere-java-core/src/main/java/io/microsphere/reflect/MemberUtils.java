9f13e619d8d4ef1769548c28c8abca31cfac577b
Mercy Ma <mercyblitz@gmail.com>
2025-04-02 11:58:41 +0800

    Update MemberUtils.java

--- microsphere-java-core/src/main/java/io/microsphere/reflect/MemberUtils.java
+++ microsphere-java-core/src/main/java/io/microsphere/reflect/MemberUtils.java
@@ -16,6 +16,8 @@
  */
 package io.microsphere.reflect;
 
+import io.microsphere.util.BaseUtils;
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Member;
@@ -28,7 +30,7 @@ import java.util.function.Predicate;
  *
  * @since 1.0.0
  */
-public abstract class MemberUtils {
+public abstract class MemberUtils extends BaseUtils {
 
     /**
      * The {@link Predicate} reference to {@link #isStatic(Member)}
