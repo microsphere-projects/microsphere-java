681c6654577823d1dba5524530a545b9b7eda258
Mercy Ma <mercyblitz@gmail.com>
2025-04-02 11:16:19 +0800

    Update ManagementUtilsTest.java

--- microsphere-java-core/src/test/java/io/microsphere/management/ManagementUtilsTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/management/ManagementUtilsTest.java
@@ -1,9 +1,14 @@
 package io.microsphere.management;
 
 import io.microsphere.AbstractTestCase;
+import io.microsphere.process.ProcessIdResolver;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static io.microsphere.management.ManagementUtils.getCurrentProcessId;
+import static io.microsphere.process.ProcessIdResolver.UNKNOWN_PROCESS_ID;
+import static io.microsphere.util.ServiceLoaderUtils.loadServicesList;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
@@ -19,6 +24,14 @@ public class ManagementUtilsTest extends AbstractTestCase {
     public void testGetCurrentProcessId() {
         long currentProcessId = getCurrentProcessId();
         assertTrue(currentProcessId > 0);
+    }
+
+    @Test
+    public void testLog() {
+        List<ProcessIdResolver> resolvers = loadServicesList(ProcessIdResolver.class);
+        for(ProcessIdResolver resolver : resolvers){
+            ManagementUtils.log(resolver, UNKNOWN_PROCESS_ID);
+        }
     }
 
 }
