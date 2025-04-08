b27614912915b91904ee83e2d4de9974f64da574
Mercy Ma <mercyblitz@gmail.com>
2025-04-08 22:27:06 +0800

    Create BaseJavaTypeTest.java

---
+++ microsphere-java-core/src/test/java/io/microsphere/reflect/BaseJavaTypeTest.java
@@ -0,0 +1,205 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.microsphere.reflect;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+import static io.microsphere.reflect.JavaType.EMPTY_JAVA_TYPE_ARRAY;
+import static io.microsphere.reflect.JavaType.Kind;
+import static io.microsphere.reflect.JavaType.Kind.CLASS;
+import static io.microsphere.reflect.JavaType.Kind.GENERIC_ARRAY_TYPE;
+import static io.microsphere.reflect.JavaType.Kind.PARAMETERIZED_TYPE;
+import static io.microsphere.reflect.JavaType.Kind.TYPE_VARIABLE;
+import static io.microsphere.reflect.JavaType.Kind.UNKNOWN;
+import static io.microsphere.reflect.JavaType.Kind.WILDCARD_TYPE;
+import static io.microsphere.reflect.JavaType.OBJECT_JAVA_TYPE;
+import static io.microsphere.reflect.JavaType.from;
+import static java.util.Objects.hash;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Base {@link JavaType} for {@link Class}
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @since JavaType
+ */
+public abstract class BaseJavaTypeTest {
+
+    protected JavaType javaType;
+
+    @BeforeAll
+    public static void beforeAll() {
+        assertArrayEquals(new JavaType[0], EMPTY_JAVA_TYPE_ARRAY);
+        assertEquals(from(Object.class, CLASS), OBJECT_JAVA_TYPE);
+        assertEquals(Object.class, OBJECT_JAVA_TYPE.getType());
+        assertEquals(CLASS, OBJECT_JAVA_TYPE.getKind());
+    }
+
+    @BeforeEach
+    public void init() {
+        javaType = createJavaType();
+    }
+
+    protected JavaType createJavaType() {
+        return from(type(), kind(), source());
+    }
+
+    protected abstract Type type();
+
+    protected abstract Kind kind();
+
+    protected abstract JavaType source();
+
+    @Test
+    public void test() {
+        testType();
+        testKind();
+        testSource();
+        testEquals();
+        testHashCode();
+        testToString();
+        testGetSuperType(javaType.getSuperType());
+        testGetInterfaces(javaType.getInterfaces());
+        JavaType[] genericTypes = javaType.getGenericTypes();
+        testGetGenericTypes(genericTypes);
+        for (int i = 0; i < genericTypes.length; i++) {
+            testGetGenericType(genericTypes[i], i);
+        }
+        testGetRootSource(javaType.getRootSource());
+        testAs();
+    }
+
+    protected void testKind() {
+        Kind kind = javaType.getKind();
+        assertEquals(kind(), kind);
+        assertEquals(CLASS == kind, javaType.isClass());
+        assertEquals(PARAMETERIZED_TYPE == kind, javaType.isParameterizedType());
+        assertEquals(TYPE_VARIABLE == kind, javaType.isTypeVariable());
+        assertEquals(WILDCARD_TYPE == kind, javaType.isWildCardType());
+        assertEquals(GENERIC_ARRAY_TYPE == kind, javaType.isGenericArrayType());
+        assertEquals(UNKNOWN == kind, javaType.isUnknownType());
+    }
+
+    protected void testType() {
+        Type type = type();
+        assertEquals(type, javaType.getType());
+        Kind kind = javaType.getKind();
+        switch (kind) {
+            case CLASS:
+                assertEquals(type, javaType.getRawType());
+                assertEquals(type, javaType.toClass());
+                assertNull(javaType.toParameterizedType());
+                assertNull(javaType.toTypeVariable());
+                assertNull(javaType.toWildcardType());
+                assertNull(javaType.toGenericArrayType());
+                break;
+            case PARAMETERIZED_TYPE:
+                assertEquals(((ParameterizedType) type).getRawType(), javaType.getRawType());
+                assertNull(javaType.toClass());
+                assertEquals(type, javaType.toParameterizedType());
+                assertNull(javaType.toTypeVariable());
+                assertNull(javaType.toWildcardType());
+                assertNull(javaType.toGenericArrayType());
+                break;
+            case TYPE_VARIABLE:
+                assertNull(javaType.getRawType());
+                assertNull(javaType.toClass());
+                assertNull(javaType.toParameterizedType());
+                assertEquals(type, javaType.toTypeVariable());
+                assertNull(javaType.toWildcardType());
+                assertNull(javaType.toGenericArrayType());
+                break;
+            case WILDCARD_TYPE:
+                assertNull(javaType.getRawType());
+                assertNull(javaType.toClass());
+                assertNull(javaType.toParameterizedType());
+                assertNull(javaType.toTypeVariable());
+                assertEquals(type, javaType.toWildcardType());
+                assertNull(javaType.toGenericArrayType());
+                break;
+            case GENERIC_ARRAY_TYPE:
+                assertNull(javaType.getRawType());
+                assertNull(javaType.toClass());
+                assertNull(javaType.toParameterizedType());
+                assertNull(javaType.toTypeVariable());
+                assertNull(javaType.toWildcardType());
+                assertEquals(type, javaType.toGenericArrayType());
+                break;
+        }
+    }
+
+    protected void testSource() {
+        JavaType source = source();
+        assertEquals(source, javaType.getSource());
+        assertEquals(source == null, javaType.isRootSource());
+        assertEquals(source == null, javaType.isSource());
+        assertTrue(source.isSource());
+    }
+
+    protected void testEquals() {
+        Type type = type();
+        Kind kind = kind();
+        JavaType source = source();
+
+        assertEquals(from(type, kind, source), javaType);
+        assertEquals(source == null, from(type, kind).equals(javaType));
+        assertNotEquals(from(type, kind, javaType), javaType);
+        assertNotEquals(from(type, UNKNOWN, source), javaType);
+        assertNotEquals(from(Object.class, kind, source), javaType);
+    }
+
+    protected void testHashCode() {
+        Type type = type();
+        Kind kind = kind();
+        JavaType source = source();
+        assertEquals(hash(type, kind, source), javaType.hashCode());
+    }
+
+    protected void testToString() {
+        Type type = type();
+        Kind kind = kind();
+        JavaType source = source();
+        StringBuilder sb = new StringBuilder("JavaType{");
+        sb.append("type=").append(type);
+        sb.append(", kind=").append(kind);
+        sb.append(", source=").append(source);
+        sb.append('}');
+        assertEquals(sb.toString(), javaType.toString());
+    }
+
+    protected abstract void testGetSuperType(JavaType superType);
+
+    protected abstract void testGetInterfaces(JavaType[] interfaces);
+
+    protected abstract void testGetGenericTypes(JavaType[] genericTypes);
+
+    protected abstract void testGetGenericType(JavaType genericType, int i);
+
+    protected abstract void testAs();
+
+    protected abstract void testGetRootSource(JavaType rootSource);
+
+}
