2d1dfffa8f5d00cb46d56311214831c2345da0b1
Mercy Ma <mercyblitz@gmail.com>
2025-04-10 19:33:12 +0800

    Create ErrorArtifactResourceResolverTest.java

---
+++ microsphere-java-core/src/test/java/io/microsphere/classloading/ErrorArtifactResourceResolverTest.java
@@ -0,0 +1,18 @@
+package io.microsphere.classloading;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+/**
+ * {@link ErrorArtifactResourceResolver} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see ErrorArtifactResourceResolver
+ * @since 1.0.0
+ */
+public class ErrorArtifactResourceResolverTest extends AbstractArtifactResourceResolverTest<ErrorArtifactResourceResolver> {
+
+    @Override
+    protected void assertArtifact(Artifact artifact) throws Throwable {
+        assertNull(artifact);
+    }
+}
