db12f692240bb15ed451bbc58781791fb0732a08
Mercy Ma <mercyblitz@gmail.com>
2025-04-11 11:49:24 +0800

    Create ManifestArtifactResourceResolverTest.java

---
+++ microsphere-java-core/src/test/java/io/microsphere/classloading/ManifestArtifactResourceResolverTest.java
@@ -0,0 +1,19 @@
+package io.microsphere.classloading;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * {@link ManifestArtifactResourceResolver} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see ManifestArtifactResourceResolver
+ * @since 1.0.0
+ */
+public class ManifestArtifactResourceResolverTest extends AbstractArtifactResourceResolverTest<ManifestArtifactResourceResolver> {
+
+    @Override
+    protected void assertArtifactFromFile(Artifact artifact) throws Throwable {
+        assertEquals("FindBugs-jsr305", artifact.getArtifactId());
+        assertEquals("3.0.2", artifact.getVersion());
+    }
+}
