2fe907ffc05309fac3e9bfa195b8dd49fb105031
Mercy Ma <mercyblitz@gmail.com>
2025-04-15 22:16:53 +0800

    Update FieldUtilsTest.java

--- microsphere-java-core/src/test/java/io/microsphere/reflect/FieldUtilsTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/reflect/FieldUtilsTest.java
@@ -16,6 +16,7 @@
  */
 package io.microsphere.reflect;
 
+import io.microsphere.junit.jupiter.api.extension.annotation.UtilsTestExtension;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
@@ -48,6 +49,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @since 1.0.0
  */
+@UtilsTestExtension
 public class FieldUtilsTest {
 
     private static String value = "1";
@@ -58,7 +60,7 @@ public class FieldUtilsTest {
     public void destroy() {
         ReflectionTest.staticField = "staticField";
     }
-    
+
     @Test
     public void testFindFieldOnObject() {
         assertFindField(test, "privateField");
