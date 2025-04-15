af40f7bf0cf0d672d3eff26a5c1e82afd87885e0
Mercy Ma <mercyblitz@gmail.com>
2025-04-15 21:49:09 +0800

    Update FieldUtils.java

--- microsphere-java-core/src/main/java/io/microsphere/reflect/FieldUtils.java
+++ microsphere-java-core/src/main/java/io/microsphere/reflect/FieldUtils.java
@@ -17,7 +17,7 @@
 package io.microsphere.reflect;
 
 import io.microsphere.logging.Logger;
-import io.microsphere.util.BaseUtils;
+import io.microsphere.util.Utils;
 
 import java.lang.reflect.Field;
 import java.util.LinkedHashSet;
@@ -42,7 +42,7 @@ import static io.microsphere.util.ClassUtils.getTypeName;
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @since 1.0.0
  */
-public abstract class FieldUtils extends BaseUtils {
+public abstract class FieldUtils implements Utils {
 
     private static final Logger logger = getLogger(FieldUtils.class);
 
