5af53113b2ad5551500a3cfedfef73acb7d3b460
Mercy Ma <mercyblitz@gmail.com>
2025-04-15 22:08:23 +0800

    Update ArrayUtils.java

--- microsphere-java-core/src/main/java/io/microsphere/util/ArrayUtils.java
+++ microsphere-java-core/src/main/java/io/microsphere/util/ArrayUtils.java
@@ -42,7 +42,7 @@ import static java.util.Collections.list;
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @since 1.0.0
  */
-public abstract class ArrayUtils {
+public abstract class ArrayUtils implements Utils {
 
     /**
      * An empty immutable {@code boolean} array.
