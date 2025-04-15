2085f7c287f633f9eb007bd3cebc493a8b79cb65
Mercy Ma <mercyblitz@gmail.com>
2025-04-15 21:48:29 +0800

    Update PropertiesUtils.java

--- microsphere-java-core/src/main/java/io/microsphere/collection/PropertiesUtils.java
+++ microsphere-java-core/src/main/java/io/microsphere/collection/PropertiesUtils.java
@@ -16,7 +16,7 @@
  */
 package io.microsphere.collection;
 
-import io.microsphere.util.BaseUtils;
+import io.microsphere.util.Utils;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -32,7 +32,7 @@ import static java.util.Collections.unmodifiableMap;
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @since 1.0.0
  */
-public abstract class PropertiesUtils extends BaseUtils {
+public abstract class PropertiesUtils implements Utils {
 
     /**
      * Get the flatten the specified {@link Map properties}
