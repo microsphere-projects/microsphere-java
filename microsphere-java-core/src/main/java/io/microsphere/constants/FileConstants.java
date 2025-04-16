5314857efebd1e443834acc18b9a807ad99c5ba8
Mercy Ma <mercyblitz@gmail.com>
2025-04-16 21:35:59 +0800

    Update FileConstants.java

--- microsphere-java-core/src/main/java/io/microsphere/constants/FileConstants.java
+++ microsphere-java-core/src/main/java/io/microsphere/constants/FileConstants.java
@@ -60,12 +60,12 @@ public interface FileConstants {
     String JAR_EXTENSION = FILE_EXTENSION + JAR;
 
     /**
-     * War File extension : ".jar"
+     * War File extension : ".war"
      */
     String WAR_EXTENSION = FILE_EXTENSION + WAR;
 
     /**
-     * Ear File extension : ".jar"
+     * Ear File extension : ".ear"
      */
     String EAR_EXTENSION = FILE_EXTENSION + EAR;
 
