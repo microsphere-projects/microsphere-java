0dffc7ae331fcc860d5f353586f1954cde271f98
Mercy Ma <mercyblitz@gmail.com>
2025-05-25 18:40:58 +0800

    Update ElementUtilsTest.java

--- microsphere-annotation-processor/src/test/java/io/microsphere/annotation/processor/util/ElementUtilsTest.java
+++ microsphere-annotation-processor/src/test/java/io/microsphere/annotation/processor/util/ElementUtilsTest.java
@@ -18,23 +18,41 @@
 package io.microsphere.annotation.processor.util;
 
 
+import io.microsphere.annotation.processor.AbstractAnnotationProcessingTest;
 import org.junit.jupiter.api.Test;
 
+import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.VariableElement;
 import java.lang.annotation.ElementType;
+import java.util.List;
 
+import static io.microsphere.annotation.processor.util.ElementUtils.filterElements;
+import static io.microsphere.annotation.processor.util.ElementUtils.hasModifiers;
 import static io.microsphere.annotation.processor.util.ElementUtils.isClass;
 import static io.microsphere.annotation.processor.util.ElementUtils.isDeclaredType;
 import static io.microsphere.annotation.processor.util.ElementUtils.isExecutable;
 import static io.microsphere.annotation.processor.util.ElementUtils.isField;
 import static io.microsphere.annotation.processor.util.ElementUtils.isInitializer;
 import static io.microsphere.annotation.processor.util.ElementUtils.isInterface;
+import static io.microsphere.annotation.processor.util.ElementUtils.isPublicNonStatic;
 import static io.microsphere.annotation.processor.util.ElementUtils.isVariable;
+import static io.microsphere.annotation.processor.util.ElementUtils.matchParameterTypeNames;
+import static io.microsphere.annotation.processor.util.ElementUtils.matchParameterTypes;
 import static io.microsphere.annotation.processor.util.ElementUtils.matches;
+import static io.microsphere.annotation.processor.util.ElementUtils.matchesElementKind;
 import static io.microsphere.annotation.processor.util.ElementUtils.toElementKind;
+import static io.microsphere.annotation.processor.util.MemberUtils.getAllDeclaredMembers;
+import static io.microsphere.annotation.processor.util.MemberUtils.getDeclaredMembers;
+import static io.microsphere.annotation.processor.util.MethodUtils.findMethod;
+import static io.microsphere.collection.ListUtils.ofList;
+import static io.microsphere.lang.function.Predicates.alwaysFalse;
+import static io.microsphere.lang.function.Predicates.alwaysTrue;
 import static java.lang.annotation.ElementType.PACKAGE;
 import static java.lang.annotation.ElementType.TYPE_USE;
 import static java.lang.annotation.ElementType.values;
+import static java.util.Collections.emptyList;
 import static javax.lang.model.element.ElementKind.ANNOTATION_TYPE;
 import static javax.lang.model.element.ElementKind.CLASS;
 import static javax.lang.model.element.ElementKind.CONSTRUCTOR;
@@ -50,6 +68,9 @@ import static javax.lang.model.element.ElementKind.OTHER;
 import static javax.lang.model.element.ElementKind.PARAMETER;
 import static javax.lang.model.element.ElementKind.RESOURCE_VARIABLE;
 import static javax.lang.model.element.ElementKind.STATIC_INIT;
+import static javax.lang.model.element.Modifier.PRIVATE;
+import static javax.lang.model.util.ElementFilter.fieldsIn;
+import static javax.lang.model.util.ElementFilter.methodsIn;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
@@ -62,7 +83,53 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * @see ElementUtils
  * @since 1.0.0
  */
-class ElementUtilsTest {
+class ElementUtilsTest extends AbstractAnnotationProcessingTest {
+
+    private ExecutableElement echoMethod;
+
+    @Override
+    protected void beforeTest() {
+        super.beforeTest();
+        this.echoMethod = findMethod(testTypeElement, "echo", "java.lang.String");
+    }
+
+    @Test
+    public void testMatchesElementKind() {
+        assertTrue(matchesElementKind(echoMethod, METHOD));
+        assertFalse(matchesElementKind(echoMethod, FIELD));
+    }
+
+    @Test
+    public void testMatchesElementKindOnNull() {
+        assertFalse(matchesElementKind(NULL_ELEMENT, FIELD));
+        assertFalse(matchesElementKind(echoMethod, NULL_ELEMENT_KIND));
+    }
+
+    @Test
+    public void testIsPublicNonStatic() {
+        methodsIn(getDeclaredMembers(testTypeElement)).forEach(method -> assertTrue(isPublicNonStatic(method)));
+
+        // Integer#valueOf(String) is a public static method
+        assertFalse(isPublicNonStatic(findMethod(getTypeElement(Integer.class), "valueOf", String.class)));
+    }
+
+    @Test
+    public void testIsPublicNonStaticOnNull() {
+        assertFalse(isPublicNonStatic(NULL_ELEMENT));
+    }
+
+    @Test
+    public void testHasModifiers() {
+        List<? extends Element> members = getAllDeclaredMembers(testTypeElement.asType());
+        List<VariableElement> fields = fieldsIn(members);
+        assertTrue(hasModifiers(fields.get(0), PRIVATE));
+    }
+
+    @Test
+    public void testHasModifiersOnNull() {
+        assertFalse(hasModifiers(NULL_ELEMENT));
+        assertFalse(hasModifiers(testTypeElement, null));
+    }
 
     @Test
     public void testIsClass() {
@@ -197,6 +264,49 @@ class ElementUtilsTest {
         assertFalse(matches(null, (ElementType[]) null));
         assertFalse(matches(null, TYPE_USE, PACKAGE));
         assertFalse(matches(toElementKind(TYPE_USE), (ElementType[]) null));
+    }
+
+    @Test
+    public void testFilterElements() {
+        assertEmptyList(filterElements(ofList(testTypeElement), alwaysFalse()));
+    }
+
+    @Test
+    public void testFilterElementsOnNull() {
+        assertEmptyList(filterElements(NULL_LIST, alwaysTrue()));
+        List<ExecutableElement> methods = ofList(echoMethod);
+        assertSame(methods, filterElements(methods, NULL_PREDICATE_ARRAY));
+    }
+
+    @Test
+    public void testFilterElementsOnEmpty() {
+        assertEmptyList(filterElements(emptyList(), alwaysTrue()));
+        List<ExecutableElement> methods = ofList(echoMethod);
+        assertSame(methods, filterElements(methods));
+    }
+
+    @Test
+    public void testMatchParameterTypes() {
+        assertTrue(matchParameterTypes(echoMethod.getParameters(), String.class));
+        assertFalse(matchParameterTypes(echoMethod.getParameters(), Object.class));
+    }
+
+    @Test
+    public void testMatchParameterTypesOnNull() {
+        assertFalse(matchParameterTypes(NULL_LIST, String.class));
+        assertFalse(matchParameterTypes(emptyList(), NULL_CLASS_ARRAY));
+    }
+
+    @Test
+    public void testMatchParameterTypeNames() {
+        assertTrue(matchParameterTypeNames(echoMethod.getParameters(), "java.lang.String"));
+        assertFalse(matchParameterTypeNames(echoMethod.getParameters(), "java.lang.Object"));
+    }
+
+    @Test
+    public void testMatchParameterTypeNamesOnNull() {
+        assertFalse(matchParameterTypeNames(NULL_LIST, "java.lang.String"));
+        assertFalse(matchParameterTypeNames(emptyList(), NULL_STRING_ARRAY));
     }
 
     void assertElementKind(ElementType elementType) {
