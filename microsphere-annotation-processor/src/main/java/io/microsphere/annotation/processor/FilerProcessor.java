3ca69de6a94d312efb4162de376fbedaf0349827
Mercy Ma <mercyblitz@gmail.com>
2025-06-09 16:38:37 +0800

    Create FilerProcessor.java

---
+++ microsphere-annotation-processor/src/main/java/io/microsphere/annotation/processor/FilerProcessor.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.annotation.processor;
+
+import io.microsphere.lang.function.ThrowableFunction;
+
+import javax.annotation.processing.Filer;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.tools.JavaFileManager;
+import java.util.function.BiFunction;
+
+import static io.microsphere.annotation.processor.util.MessagerUtils.printMandatoryWarning;
+import static io.microsphere.reflect.FieldUtils.getFieldValue;
+
+/**
+ * The {@link Filer} Processor
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see ProcessingEnvironment
+ * @see Filer
+ * @since 1.0.0
+ */
+public class FilerProcessor {
+
+    private final ProcessingEnvironment processingEnv;
+
+    public FilerProcessor(ProcessingEnvironment processingEnv) {
+        this.processingEnv = processingEnv;
+    }
+
+    public <T> T processInFiler(ThrowableFunction<Filer, T> filerCallback) {
+        return processInFiler(filerCallback, (filer, e) -> {
+            printMandatoryWarning(this.processingEnv, "[FilerProcessor] Failed to process in Filer : {}", e.getMessage());
+            return null;
+        });
+    }
+
+    public <T> T processInFiler(ThrowableFunction<Filer, T> filerCallback, BiFunction<Filer, Throwable, T> exceptionHandler) {
+        Filer filer = processingEnv.getFiler();
+        return filerCallback.execute(filer, exceptionHandler);
+    }
+
+    /**
+     * Get the {@link JavaFileManager}
+     *
+     * @return the {@link JavaFileManager}
+     */
+    public JavaFileManager getJavaFileManager() {
+        return processInFiler(filer -> getFieldValue(filer, "fileManager"));
+    }
+
+}
