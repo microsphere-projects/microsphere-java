19090d7debd9883ef4880a2364f5099935663275
Mercy Ma <mercyblitz@gmail.com>
2025-06-26 13:02:55 +0800

    Update UnmodifiableDeque.java

--- microsphere-java-core/src/main/java/io/microsphere/collection/UnmodifiableDeque.java
+++ microsphere-java-core/src/main/java/io/microsphere/collection/UnmodifiableDeque.java
@@ -24,14 +24,34 @@ import java.util.Iterator;
 import static io.microsphere.collection.CollectionUtils.unmodifiableIterator;
 
 /**
- * An unmodifiable view of a {@link Deque} which wraps a given delegate and prevents any modifications to it.
+ * An unmodifiable view of a {@link Deque}. This implementation decorates a given deque and prevents any
+ * modification operations from succeeding. All mutation methods, such as {@link #addFirst}, {@link #addLast},
+ * {@link #push}, and others, throw an {@link UnsupportedOperationException}.
+ *
  * <p>
- * All mutation operations like {@link #addFirst(Object)}, {@link #addLast(Object)}, or {@link #clear()} will throw an
- * {@link UnsupportedOperationException}.
+ * The behavior of this class is undefined if the underlying deque is modified directly while this view exists.
+ * It is intended to be used for creating immutable snapshots of deques where modification attempts are not allowed.
+ * </p>
+ *
+ * <p>
+ * Example usage:
+ * </p>
+ *
+ * <pre>{@code
+ * Deque<String> mutableDeque = new LinkedList<>();
+ * mutableDeque.addFirst("World");
+ * Deque<String> unmodifiableDeque = new UnmodifiableDeque<>(mutableDeque);
+ * unmodifiableDeque.addFirst("Hello"); // throws UnsupportedOperationException
+ *  }</pre>
+ *
+ * <p>
+ * This class is serializable if the underlying deque is serializable.
  * </p>
  *
  * @param <E> the type of elements held in this deque
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * @see Deque
+ * @see CollectionUtils#unmodifiableIterator(Iterator)
  * @since 1.0.0
  */
 public class UnmodifiableDeque<E> extends UnmodifiableQueue<E> implements Deque<E>, Serializable {
