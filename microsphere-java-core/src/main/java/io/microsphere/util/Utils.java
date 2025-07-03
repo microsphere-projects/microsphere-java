2f5bff040549c65fd9d54f94df62a25a2a68d896
Mercy Ma <mercyblitz@gmail.com>
2025-07-03 10:33:36 +0800

    Update Utils.java

--- microsphere-java-core/src/main/java/io/microsphere/util/Utils.java
+++ microsphere-java-core/src/main/java/io/microsphere/util/Utils.java
@@ -17,7 +17,22 @@
 package io.microsphere.util;
 
 /**
- * The marker interface for the utilities class
+ * A marker interface serving as a base for utility classes within the MicroSphere framework.
+ * <p>
+ * This interface is intended to be extended by utility classes that provide static methods
+ * for common operations. It helps in organizing utility classes under a common type hierarchy.
+ * </p>
+ *
+ * <h3>Example Usage</h3>
+ * <pre>
+ * public final class MyUtils implements Utils {
+ *     private MyUtils() {}
+ *
+ *     public static void doSomething() {
+ *         // Utility logic here
+ *     }
+ * }
+ * </pre>
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @see BaseUtils
