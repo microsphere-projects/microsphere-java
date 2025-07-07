2a35b8b5db4fb89b9d35a7d5eb0b5501411974c4
Mercy Ma <mercyblitz@gmail.com>
2025-07-07 20:40:22 +0800

    Update DelegatingDeque.java

--- microsphere-java-core/src/main/java/io/microsphere/collection/DelegatingDeque.java
+++ microsphere-java-core/src/main/java/io/microsphere/collection/DelegatingDeque.java
@@ -16,21 +16,13 @@
  */
 package io.microsphere.collection;
 
-import io.microsphere.lang.DelegatingWrapper;
-
 import java.lang.invoke.MethodHandle;
-import java.util.Collection;
 import java.util.Deque;
 import java.util.Iterator;
-import java.util.Spliterator;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
 
 import static io.microsphere.collection.QueueUtils.reversedDeque;
 import static io.microsphere.invoke.MethodHandlesLookupUtils.findPublicVirtual;
 import static io.microsphere.lang.function.ThrowableSupplier.execute;
-import static io.microsphere.util.Assert.assertNotNull;
 
 /**
  * Delegating {@link Deque}
@@ -39,7 +31,7 @@ import static io.microsphere.util.Assert.assertNotNull;
  * @see AbstractDeque
  * @since 1.0.0
  */
-public class DelegatingDeque<E> implements Deque<E>, DelegatingWrapper {
+public class DelegatingDeque<E> extends DelegatingQueue<E> implements Deque<E> {
 
     /**
      * The MethodHandle for {@link Deque#reversed()} since Java 21
@@ -49,7 +41,7 @@ public class DelegatingDeque<E> implements Deque<E>, DelegatingWrapper {
     protected final Deque<E> delegate;
 
     public DelegatingDeque(Deque<E> delegate) {
-        assertNotNull(delegate, () -> "The 'delegate' argument must not be null!");
+        super(delegate);
         this.delegate = delegate;
     }
 
@@ -124,41 +116,6 @@ public class DelegatingDeque<E> implements Deque<E>, DelegatingWrapper {
     }
 
     @Override
-    public boolean add(E e) {
-        return delegate.add(e);
-    }
-
-    @Override
-    public boolean offer(E e) {
-        return delegate.offer(e);
-    }
-
-    @Override
-    public E remove() {
-        return delegate.remove();
-    }
-
-    @Override
-    public E poll() {
-        return delegate.poll();
-    }
-
-    @Override
-    public E element() {
-        return delegate.element();
-    }
-
-    @Override
-    public E peek() {
-        return delegate.peek();
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends E> c) {
-        return delegate.addAll(c);
-    }
-
-    @Override
     public void push(E e) {
         delegate.push(e);
     }
@@ -169,108 +126,8 @@ public class DelegatingDeque<E> implements Deque<E>, DelegatingWrapper {
     }
 
     @Override
-    public boolean remove(Object o) {
-        return delegate.remove(o);
-    }
-
-    @Override
-    public boolean contains(Object o) {
-        return delegate.contains(o);
-    }
-
-    @Override
-    public int size() {
-        return delegate.size();
-    }
-
-    @Override
-    public Iterator<E> iterator() {
-        return delegate.iterator();
-    }
-
-    @Override
     public Iterator<E> descendingIterator() {
         return delegate.descendingIterator();
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return delegate.isEmpty();
-    }
-
-    @Override
-    public Object[] toArray() {
-        return delegate.toArray();
-    }
-
-    @Override
-    public <T> T[] toArray(T[] a) {
-        return delegate.toArray(a);
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        return delegate.containsAll(c);
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        return delegate.removeAll(c);
-    }
-
-    @Override
-    public boolean removeIf(Predicate<? super E> filter) {
-        return delegate.removeIf(filter);
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        return delegate.retainAll(c);
-    }
-
-    @Override
-    public void clear() {
-        delegate.clear();
-    }
-
-    @Override
-    public Spliterator<E> spliterator() {
-        return delegate.spliterator();
-    }
-
-    @Override
-    public Stream<E> stream() {
-        return delegate.stream();
-    }
-
-    @Override
-    public Stream<E> parallelStream() {
-        return delegate.parallelStream();
-    }
-
-    @Override
-    public void forEach(Consumer<? super E> action) {
-        delegate.forEach(action);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        return delegate.equals(o);
-    }
-
-    @Override
-    public int hashCode() {
-        return delegate.hashCode();
-    }
-
-    @Override
-    public String toString() {
-        return delegate.toString();
-    }
-
-    @Override
-    public Object getDelegate() {
-        return this.delegate;
     }
 
     /**
