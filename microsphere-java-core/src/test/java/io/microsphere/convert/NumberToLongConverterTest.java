e330628d7924d2be76aa8c86dbb189e5cafc10cf
Mercy Ma <mercyblitz@gmail.com>
2025-07-08 15:23:45 +0800

    Update NumberToLongConverterTest.java

--- microsphere-java-core/src/test/java/io/microsphere/convert/NumberToLongConverterTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/convert/NumberToLongConverterTest.java
@@ -49,7 +49,7 @@ class NumberToLongConverterTest extends BaseConverterTest<Number, Long> {
     }
 
     @Test
-    public void testCovertMore() {
+    void testCovertMore() {
         assertEquals(getTarget(), this.converter.convert(1));
     }
 }
