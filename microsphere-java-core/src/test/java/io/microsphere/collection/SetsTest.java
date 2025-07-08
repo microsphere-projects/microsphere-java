58fc06dcd32a2b0ef731ee5585a7f4b2ed4e95c9
Mercy Ma <mercyblitz@gmail.com>
2025-07-08 15:09:36 +0800

    Update SetsTest.java

--- microsphere-java-core/src/test/java/io/microsphere/collection/SetsTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/collection/SetsTest.java
@@ -15,65 +15,65 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * @see Sets
  * @since 1.0.0
  */
-public class SetsTest extends AbstractTestCase {
+class SetsTest extends AbstractTestCase {
 
     @Test
-    public void testOfSet0() {
+    void testOfSet0() {
         assertEquals(emptySet(), ofSet());
     }
 
     @Test
-    public void testOfSet1() {
+    void testOfSet1() {
         assertEquals(of(1), ofSet(1));
     }
 
     @Test
-    public void testOfSet2() {
+    void testOfSet2() {
         assertEquals(of(1, 2), ofSet(1, 2));
     }
 
     @Test
-    public void testOfSet3() {
+    void testOfSet3() {
         assertEquals(of(1, 2, 3), ofSet(1, 2, 3));
     }
 
     @Test
-    public void testOfSet4() {
+    void testOfSet4() {
         assertEquals(of(1, 2, 3, 4), ofSet(1, 2, 3, 4));
     }
 
     @Test
-    public void testOfSet5() {
+    void testOfSet5() {
         assertEquals(of(1, 2, 3, 4, 5), ofSet(1, 2, 3, 4, 5));
     }
 
     @Test
-    public void testOfSet6() {
+    void testOfSet6() {
         assertEquals(of(1, 2, 3, 4, 5, 6), ofSet(1, 2, 3, 4, 5, 6));
     }
 
     @Test
-    public void testOfSet7() {
+    void testOfSet7() {
         assertEquals(of(1, 2, 3, 4, 5, 6, 7), ofSet(1, 2, 3, 4, 5, 6, 7));
     }
 
     @Test
-    public void testOfSet8() {
+    void testOfSet8() {
         assertEquals(of(1, 2, 3, 4, 5, 6, 7, 8), ofSet(1, 2, 3, 4, 5, 6, 7, 8));
     }
 
     @Test
-    public void testOfSet9() {
+    void testOfSet9() {
         assertEquals(of(1, 2, 3, 4, 5, 6, 7, 8, 9), ofSet(1, 2, 3, 4, 5, 6, 7, 8, 9));
     }
 
     @Test
-    public void testOfSet10() {
+    void testOfSet10() {
         assertEquals(of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), ofSet(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
     }
 
     @Test
-    public void testOfSet() {
+    void testOfSet() {
         assertEquals(emptySet(), ofSet(TEST_NULL_OBJECT_ARRAY));
         assertEquals(of(1, 2, 3), ofSet(1, 2, 3));
     }
