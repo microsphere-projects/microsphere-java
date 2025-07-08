3a9b7ab632118ebd880ec88ecd4735650441188f
Mercy Ma <mercyblitz@gmail.com>
2025-07-08 17:27:27 +0800

    Create IntegerAdder.java

---
+++ microsphere-java-core/src/main/java/io/microsphere/util/IntegerAdder.java
@@ -0,0 +1,347 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.util;
+
+/**
+ * A mutable integer container that provides various atomic operations for integer addition,
+ * increment, decrement, and value retrieval. This class extends {@link Number}, allowing it to be used
+ * in contexts where a numeric representation is required.
+ *
+ * <h3>Example Usage</h3>
+ * <pre>{@code
+ * IntegerAdder adder = IntegerAdder.of(5);
+ *
+ * // Get and set a new value
+ * int oldValue = adder.getAndSet(10); // Returns 5, value becomes 10
+ *
+ * // Increment and get the new value
+ * int newValue = adder.incrementAndGet(); // Returns 11, value becomes 11
+ *
+ * // Add a delta and get the updated value
+ * int result = adder.addAndGet(5); // Returns 16, value becomes 16
+ *
+ * // Get current value
+ * int currentValue = adder.get(); // Returns 16
+ * }</pre>
+ */
+public class IntegerAdder extends Number {
+
+    private int value;
+
+    public IntegerAdder(int value) {
+        super();
+        this.value = value;
+    }
+
+    /**
+     * Sets the value to the given {@code newValue} and returns the previous value.
+     *
+     * @param newValue the new value to set
+     * @return the previous value before the update
+     *
+     * <h3>Example Usage</h3>
+     * <pre>{@code
+     * IntegerAdder adder = new IntegerAdder(5);
+     * int oldValue = adder.getAndSet(10); // sets the value to 10, returns 5
+     * System.out.println(oldValue); // prints 5
+     * System.out.println(adder.get()); // prints 10
+     * }</pre>
+     */
+    public int getAndSet(int newValue) {
+        int oldValue = this.value;
+        this.value = newValue;
+        return oldValue;
+    }
+
+    /**
+     * Sets the value to the given newValue and returns this instance.
+     *
+     * @param newValue the new value to set
+     * @return this instance of {@link IntegerAdder}
+     *
+     * <h3>Example Usage</h3>
+     * <pre>{@code
+     * IntegerAdder adder = new IntegerAdder(0);
+     * adder.set(10); // sets the value to 10
+     * System.out.println(adder.get()); // prints 10
+     * }</pre>
+     */
+    public IntegerAdder set(int newValue) {
+        this.value = newValue;
+        return this;
+    }
+
+    /**
+     * Gets the current value stored in this {@link IntegerAdder}.
+     *
+     * @return the current integer value
+     *
+     * <h3>Example Usage</h3>
+     * <pre>{@code
+     * IntegerAdder adder = new IntegerAdder(5);
+     * int currentValue = adder.get(); // retrieves the current value
+     * System.out.println(currentValue); // prints 5
+     * }</pre>
+     */
+    public int get() {
+        return value;
+    }
+
+    /**
+     * increments the current value by 1 and returns the previous value before the increment.
+     *
+     * <p>This method behaves similarly to a combination of the {@code get()} and {@code incrementAndGet()}
+     * methods, where the current value is returned before it is increased by 1.</p>
+     *
+     * @return the value before incrementing
+     *
+     * <h3>Example Usage</h3>
+     * <pre>{@code
+     * IntegerAdder adder = new IntegerAdder(5);
+     * int oldValue = adder.getAndIncrement(); // increments the value to 6, returns 5
+     * System.out.println(oldValue); // prints 5
+     * System.out.println(adder.get()); // prints 6
+     * }</pre>
+     */
+    public int getAndIncrement() {
+        return value++;
+    }
+
+    /**
+     * Decrements the current value by 1 and returns the previous value before the decrement.
+     *
+     * <p>This method behaves similarly to a combination of the {@code get()} and {@code decrementAndGet()}
+     * methods, where the current value is returned before it is decreased by 1.</p>
+     *
+     * @return the value before decrementing
+     *
+     * <h3>Example Usage</h3>
+     * <pre>{@code
+     * IntegerAdder adder = new IntegerAdder(5);
+     * int oldValue = adder.getAndDecrement(); // decrements the value to 4, returns 5
+     * System.out.println(oldValue); // prints 5
+     * System.out.println(adder.get()); // prints 4
+     * }</pre>
+     */
+    public int getAndDecrement() {
+        return value--;
+    }
+
+    /**
+     * Adds the given {@code delta} to the current value and returns the previous value before the addition.
+     *
+     * <p>This method ensures that the current value is returned before the addition operation
+     * is performed, similar to a combination of the {@code get()} and {@code addAndGet(int)} methods.</p>
+     *
+     * @param delta the value to add to the current value
+     * @return the previous value before the addition
+     *
+     * <h3>Example Usage</h3>
+     * <pre>{@code
+     * IntegerAdder adder = new IntegerAdder(5);
+     * int oldValue = adder.getAndAdd(3); // adds 3 to the current value (5), returns 5
+     * System.out.println(oldValue); // prints 5
+     * System.out.println(adder.get()); // prints 8
+     * }</pre>
+     */
+    public int getAndAdd(int delta) {
+        int oldValue = this.value;
+        this.value = oldValue + delta;
+        return oldValue;
+    }
+
+    /**
+     * Increments the current value by 1 and returns the updated value.
+     *
+     * <p>This method behaves similarly to the {@code ++value} operation,
+     * where the value is increased by 1 before it is returned.</p>
+     *
+     * @return the updated value after incrementing
+     *
+     * <h3>Example Usage</h3>
+     * <pre>{@code
+     * IntegerAdder adder = new IntegerAdder(5);
+     * int newValue = adder.incrementAndGet(); // increments the value to 6, returns 6
+     * System.out.println(newValue); // prints 6
+     * System.out.println(adder.get()); // prints 6
+     * }</pre>
+     */
+    public int incrementAndGet() {
+        return ++value;
+    }
+
+    /**
+     * Decrements the current value by 1 and returns the updated value.
+     *
+     * <p>This method behaves similarly to the {@code --value} operation,
+     * where the value is decreased by 1 before it is returned.</p>
+     *
+     * @return the updated value after decrementing
+     *
+     * <h3>Example Usage</h3>
+     * <pre>{@code
+     * IntegerAdder adder = new IntegerAdder(5);
+     * int newValue = adder.decrementAndGet(); // decrements the value to 4, returns 4
+     * System.out.println(newValue); // prints 4
+     * System.out.println(adder.get()); // prints 4
+     * }</pre>
+     */
+    public int decrementAndGet() {
+        return --value;
+    }
+
+    /**
+     * Adds the given {@code delta} to the current value and returns the updated value.
+     *
+     * <p>This method ensures that the new value is calculated and stored before it is returned,
+     * behaving similarly to the compound addition operation followed by a retrieval.</p>
+     *
+     * @param delta the value to add to the current value
+     * @return the updated value after adding the delta
+     *
+     * <h3>Example Usage</h3>
+     * <pre>{@code
+     * IntegerAdder adder = new IntegerAdder(5);
+     * int newValue = adder.addAndGet(3); // adds 3 to the current value (5), returns 8
+     * System.out.println(newValue); // prints 8
+     * System.out.println(adder.get()); // prints 8
+     * }</pre>
+     */
+    public int addAndGet(int delta) {
+        int value = this.value + delta;
+        this.value = value;
+        return value;
+    }
+
+    /**
+     * Returns the integer value stored in this {@link IntegerAdder}.
+     *
+     * <p>This method provides the implementation for the {@link Number} class's abstract method,
+     * allowing this class to be used where a {@code Number} is expected.</p>
+     *
+     * @return the current integer value
+     *
+     * <h3>Example Usage</h3>
+     * <pre>{@code
+     * IntegerAdder adder = new IntegerAdder(7);
+     * int value = adder.intValue(); // retrieves the current integer value
+     * System.out.println(value); // prints 7
+     * }</pre>
+     */
+    @Override
+    public int intValue() {
+        return get();
+    }
+
+    @Override
+    /**
+     * Returns the current value as a {@code long}.
+     *
+     * <p>This method provides the implementation for the {@link Number} class's abstract method,
+     * allowing this class to be used where a {@code Number} is expected.</p>
+     *
+     * @return the current value as a {@code long}
+     *
+     * <h3>Example Usage</h3>
+     * <pre>{@code
+     * IntegerAdder adder = new IntegerAdder(5);
+     * long value = adder.longValue(); // retrieves the current value as a long
+     * System.out.println(value); // prints 5
+     * }</pre>
+     */
+    public long longValue() {
+        return get();
+    }
+
+    /**
+     * Returns the current value as a {@code float}.
+     *
+     * <p>This method provides the implementation for the {@link Number} class's abstract method,
+     * allowing this class to be used where a {@code Number} is expected.</p>
+     *
+     * @return the current value as a {@code float}
+     *
+     * <h3>Example Usage</h3>
+     * <pre>{@code
+     * IntegerAdder adder = new IntegerAdder(5);
+     * float value = adder.floatValue(); // retrieves the current value as a float
+     * System.out.println(value); // prints 5.0
+     * }</pre>
+     */
+    @Override
+    public float floatValue() {
+        return (float) get();
+    }
+
+    /**
+     * Returns the current value as a {@code double}.
+     *
+     * <p>This method provides the implementation for the {@link Number} class's abstract method,
+     * allowing this class to be used where a {@code Number} is expected.</p>
+     *
+     * @return the current value as a {@code double}
+     *
+     * <h3>Example Usage</h3>
+     * <pre>{@code
+     * IntegerAdder adder = new IntegerAdder(5);
+     * double value = adder.doubleValue(); // retrieves the current value as a double
+     * System.out.println(value); // prints 5.0
+     * }</pre>
+     */
+    @Override
+    public double doubleValue() {
+        return (double) get();
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof IntegerAdder)) {
+            return false;
+        }
+        return value == ((IntegerAdder) obj).value;
+    }
+
+    /**
+     * Creates a new instance of {@link IntegerAdder} initialized with the given value.
+     *
+     * <p>This static factory method provides a convenient way to create an instance of
+     * {@link IntegerAdder} with the specified initial value.</p>
+     *
+     * @param value the initial value for the {@link IntegerAdder}
+     * @return a new instance of {@link IntegerAdder} initialized with the given value
+     *
+     * <h3>Example Usage</h3>
+     * <pre>{@code
+     * IntegerAdder adder = IntegerAdder.of(10); // creates an IntegerAdder with initial value 10
+     * System.out.println(adder.get()); // prints 10
+     * }</pre>
+     */
+    public static IntegerAdder of(int value) {
+        return new IntegerAdder(value);
+    }
+}
