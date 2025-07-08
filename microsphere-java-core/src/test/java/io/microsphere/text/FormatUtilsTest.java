3844c0f0780f32d06fec97faff131295b50d57f8
Mercy Ma <mercyblitz@gmail.com>
2025-07-08 15:36:14 +0800

    Update FormatUtilsTest.java

--- microsphere-java-core/src/test/java/io/microsphere/text/FormatUtilsTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/text/FormatUtilsTest.java
@@ -29,7 +29,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @since 1.0.0
  */
-public class FormatUtilsTest {
+class FormatUtilsTest {
 
     @Test
     public void testFormat() {
