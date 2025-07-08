668314354e9e174f4aae2cc6753f2054afd3b760
Mercy Ma <mercyblitz@gmail.com>
2025-07-08 15:04:25 +0800

    Update ManifestArtifactResourceResolverTest.java

--- microsphere-java-core/src/test/java/io/microsphere/classloading/ManifestArtifactResourceResolverTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/classloading/ManifestArtifactResourceResolverTest.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ManifestArtifactResourceResolverTest extends StreamArtifactResourceResolverTest<ManifestArtifactResourceResolver> {
 
     @Override
-    protected void assertArtifact(Artifact artifact) {
+    void assertArtifact(Artifact artifact) {
         assertEquals("FindBugs-jsr305", artifact.getArtifactId());
         assertEquals(TEST_VERSION, artifact.getVersion());
     }
