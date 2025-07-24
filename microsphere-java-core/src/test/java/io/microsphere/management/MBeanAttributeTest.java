e48ee04cf9491081769fbe1e8b704f84ee2e6e5b
Mercy Ma <mercyblitz@gmail.com>
2025-07-24 20:38:37 +0800

    Update MBeanAttributeTest.java

--- microsphere-java-core/src/test/java/io/microsphere/management/MBeanAttributeTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/management/MBeanAttributeTest.java
@@ -37,7 +37,7 @@ public class MBeanAttributeTest {
     private static Map<String, MBeanAttribute> mBeanAttributesMap;
 
     @BeforeAll
-    public static void setUp() throws Throwable {
+    public static void beforeAll() throws Throwable {
         mBeanServer = getPlatformMBeanServer();
         objectName = getInstance("java.lang:type=ClassLoading");
         notFoundObjectName = getInstance("java.lang:type=NotFound");
