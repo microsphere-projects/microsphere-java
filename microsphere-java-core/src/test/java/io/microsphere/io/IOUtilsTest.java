ffe2717bf3a7c3700fc332428fd68b7311a79a45
Mercy Ma <mercyblitz@gmail.com>
2025-07-24 20:38:19 +0800

    Update IOUtilsTest.java

--- microsphere-java-core/src/test/java/io/microsphere/io/IOUtilsTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/io/IOUtilsTest.java
@@ -45,7 +45,7 @@ class IOUtilsTest extends AbstractTestCase {
     }
 
     @AfterEach
-    void destroy() {
+    void tearDown() {
         inputStream.close();
     }
 
@@ -175,7 +175,7 @@ class IOUtilsTest extends AbstractTestCase {
 
     @Test
     void testClose() {
-        destroy();
+        tearDown();
         close(new FastByteArrayOutputStream(0));
     }
 
