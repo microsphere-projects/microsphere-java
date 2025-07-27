106e5ff2e5ac49db190a11b977f7eeeb062d5f74
Mercy Ma <mercyblitz@gmail.com>
2025-07-27 19:20:12 +0800

    Update BeanUtils.java

--- microsphere-java-core/src/main/java/io/microsphere/beans/BeanUtils.java
+++ microsphere-java-core/src/main/java/io/microsphere/beans/BeanUtils.java
@@ -17,6 +17,7 @@
 
 package io.microsphere.beans;
 
+import io.microsphere.collection.MapUtils;
 import io.microsphere.util.Utils;
 
 import java.beans.BeanInfo;
@@ -24,13 +25,22 @@ import java.beans.PropertyDescriptor;
 import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import static io.microsphere.collection.CollectionUtils.size;
+import static io.microsphere.collection.ListUtils.isList;
 import static io.microsphere.collection.ListUtils.newArrayList;
+import static io.microsphere.collection.MapUtils.isMap;
+import static io.microsphere.collection.MapUtils.newFixedLinkedHashMap;
 import static io.microsphere.collection.MapUtils.newHashMap;
+import static io.microsphere.collection.SetUtils.isSet;
+import static io.microsphere.collection.SetUtils.newFixedLinkedHashSet;
 import static io.microsphere.reflect.AccessibleObjectUtils.trySetAccessible;
 import static io.microsphere.reflect.MethodUtils.invokeMethod;
+import static io.microsphere.util.ClassUtils.isCharSequence;
+import static io.microsphere.util.ClassUtils.isClass;
+import static io.microsphere.util.ClassUtils.isNumber;
 import static io.microsphere.util.ClassUtils.isSimpleType;
-import static io.microsphere.util.ClassUtils.isWrapperType;
 import static io.microsphere.util.StringUtils.uncapitalize;
 import static java.beans.Introspector.getBeanInfo;
 import static java.lang.reflect.Array.get;
@@ -45,54 +55,90 @@ import static java.util.Collections.unmodifiableMap;
  * @see Utils
  * @since 1.0.0
  */
-public class BeanUtils implements Utils {
+public abstract class BeanUtils implements Utils {
 
-    public static Map<String, Object> readPropertiesAsMap(Object bean) {
+    protected static Map<String, Object> resolvePropertiesAsMap(Object bean) {
         if (bean == null) {
             return emptyMap();
         }
 
-        Map<String, Object> propertiesMap = newHashMap();
-
         Class<?> beanClass = bean.getClass();
+        Map<String, Object> propertiesMap = newHashMap();
         try {
             BeanInfo beanInfo = getBeanInfo(beanClass, Object.class);
             PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
             for (int i = 0; i < propertyDescriptors.length; i++) {
                 PropertyDescriptor propertyDescriptor = propertyDescriptors[i];
                 String propertyName = uncapitalize(propertyDescriptor.getName());
-                Object propertyValue = readProperty(bean, propertyDescriptor);
+                Object propertyValue = resolveProperty(bean, propertyDescriptor);
                 propertiesMap.put(propertyName, propertyValue);
             }
         } catch (Throwable e) {
             throw new RuntimeException(e);
         }
-
         return unmodifiableMap(propertiesMap);
     }
 
-    protected static Object readProperty(Object instance, PropertyDescriptor propertyDescriptor) throws Throwable {
+    static Object resolveProperty(Object instance, PropertyDescriptor propertyDescriptor) {
         Method readMethod = propertyDescriptor.getReadMethod();
         trySetAccessible(readMethod);
         Object propertyValue = invokeMethod(instance, readMethod);
-
         Class<?> propertyType = propertyDescriptor.getPropertyType();
-        if (propertyType.isPrimitive()
-                || propertyType.isEnum()
-                || isWrapperType(propertyType)
-                || isSimpleType(propertyType)
-        ) {
+        return resolveProperty(propertyValue, propertyType);
+    }
 
-        } else if (propertyType.isArray()) {
-            int length = propertyValue == null ? 0 : getLength(propertyValue);
-            List<Object> values = newArrayList(length);
-            for (int i = 0; i < length; i++) {
-                Object element = get(propertyValue, i);
-                values.add(i, readPropertiesAsMap(element));
-            }
+    static Object resolveProperty(Object value) {
+        return resolveProperty(value, value == null ? null : value.getClass());
+    }
+
+    static Object resolveProperty(Object value, Class<?> valueType) {
+        if (value == null) {
+            return value;
         }
 
-        return propertyValue;
+        Object resolvedValue = value;
+
+        if (valueType.isPrimitive()
+                || isSimpleType(valueType)
+                || isCharSequence(valueType)
+                || isNumber(valueType)
+                || valueType.isEnum()
+                || isClass(value)
+        ) {
+            // do nothing, just return the propertyValue
+        } else if (valueType.isArray()) {
+            int length = resolvedValue == null ? 0 : getLength(resolvedValue);
+            List<Object> values = newArrayList(length);
+            for (int i = 0; i < length; i++) {
+                Object element = get(resolvedValue, i);
+                values.add(i, resolvePropertiesAsMap(element));
+            }
+        } else if (isList(valueType)) {
+            List<?> list = (List<?>) resolvedValue;
+            int size = size(list);
+            List<Object> newList = newArrayList(size);
+            for (int i = 0; i < size; i++) {
+                Object element = list.get(i);
+                newList.add(i, resolveProperty(element));
+            }
+        } else if (isSet(valueType)) {
+            Set<?> set = (Set<?>) resolvedValue;
+            Set<Object> newSet = newFixedLinkedHashSet(size(set));
+            for (Object element : set) {
+                newSet.add(resolveProperty(element));
+            }
+        } else if (isMap(valueType)) {
+            Map<?, ?> map = (Map<?, ?>) resolvedValue;
+            Map<Object, Object> newMap = newFixedLinkedHashMap(MapUtils.size(map));
+            for (Map.Entry<?, ?> entry : map.entrySet()) {
+                newMap.put(entry.getKey(), resolveProperty(entry.getValue()));
+            }
+            resolvedValue = newMap;
+        } else { // as the POJO
+            resolvedValue = resolvePropertiesAsMap(resolvedValue);
+        }
+
+        return resolvedValue;
     }
 
     private BeanUtils() {
