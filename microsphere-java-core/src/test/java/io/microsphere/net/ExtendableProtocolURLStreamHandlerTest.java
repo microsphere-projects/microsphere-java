1db1b6c061e0ec65b7f0b8e9e617c33e46384b8a
Mercy Ma <mercyblitz@gmail.com>
2025-07-29 14:48:27 +0800

    Update ExtendableProtocolURLStreamHandlerTest.java

--- microsphere-java-core/src/test/java/io/microsphere/net/ExtendableProtocolURLStreamHandlerTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/net/ExtendableProtocolURLStreamHandlerTest.java
@@ -219,18 +219,18 @@ class ExtendableProtocolURLStreamHandlerTest {
     }
 
     @Test
-    public void testResolvePath() throws IOException {
+    void testResolvePath() throws IOException {
         assertEquals("/abc", handler.resolvePath(new URL(TEST_URL)));
         assertEquals("/abc", handler.resolvePath(new URL(TEST_URL_WITH_SP)));
     }
 
     @Test
-    public void testGetProtocol() {
+    void testGetProtocol() {
         assertEquals("test", handler.getProtocol());
     }
 
     @Test
-    public void testToString() {
+    void testToString() {
         assertEquals("io.microsphere.net.test.Handler {defaultPort = -1 , protocol = 'test'}", handler.toString());
     }
 
