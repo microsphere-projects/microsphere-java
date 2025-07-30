9a86e23f3c2e41f946aac166e4d31c484f7a5159
Mercy Ma <mercyblitz@gmail.com>
2025-07-30 21:04:21 +0800

    Update ShutdownHookUtils.java

--- microsphere-java-core/src/main/java/io/microsphere/util/ShutdownHookUtils.java
+++ microsphere-java-core/src/main/java/io/microsphere/util/ShutdownHookUtils.java
@@ -376,6 +376,7 @@ public abstract class ShutdownHookUtils implements Utils {
         shutdownHookCallbacks.clear();
     }
 
+    @Nonnull
     private static Map<Thread, Thread> shutdownHookThreadsMap() {
         Class<?> applicationShutdownHooksClass = resolveClass(TARGET_CLASS_NAME, getSystemClassLoader());
         return applicationShutdownHooksClass == null ? emptyMap() : getStaticFieldValue(applicationShutdownHooksClass, HOOKS_FIELD_NAME);
