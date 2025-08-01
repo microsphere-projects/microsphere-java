7b2e3f46b82bd702b375e24dda1577ebbddffb75
Mercy Ma <mercyblitz@gmail.com>
2025-08-01 21:18:11 +0800

    Update Modifier.java

--- microsphere-java-core/src/main/java/io/microsphere/reflect/Modifier.java
+++ microsphere-java-core/src/main/java/io/microsphere/reflect/Modifier.java
@@ -122,35 +122,218 @@ public enum Modifier {
         return (mod & value) != 0;
     }
 
+    /**
+     * matches the specified modifiers
+     *
+     * @param modifiers the modifier
+     * @return <code>true</code> if matches, otherwise <code>false</code>
+     */
+    public static boolean isPublic(int modifiers) {
+        return PUBLIC.matches(modifiers);
+    }
 
     /**
-     * @param modifiers {@link Class#getModifiers()}
-     * @return true if this class's modifiers represents an annotation type; false otherwise
-     * @see Class#isAnnotation()
-     * @see io.microsphere.reflect.Modifier#ANNOTATION
+     * Checks if the specified modifiers contain the {@code private} modifier.
+     *
+     * @param modifiers the bit mask of modifiers to check
+     * @return {@code true} if the {@code private} modifier is present, otherwise {@code false}
+     * @see java.lang.reflect.Modifier#PRIVATE
+     * @see #PRIVATE
+     */
+    public static boolean isPrivate(int modifiers) {
+        return PRIVATE.matches(modifiers);
+    }
+
+    /**
+     * Checks if the specified modifiers contain the {@code protected} modifier.
+     *
+     * @param modifiers the bit mask of modifiers to check
+     * @return {@code true} if the {@code protected} modifier is present, otherwise {@code false}
+     * @see java.lang.reflect.Modifier#PROTECTED
+     * @see #PROTECTED
+     */
+    public static boolean isProtected(int modifiers) {
+        return PROTECTED.matches(modifiers);
+    }
+
+    /**
+     * Checks if the specified modifiers contain the {@code static} modifier.
+     *
+     * @param modifiers the bit mask of modifiers to check
+     * @return {@code true} if the {@code static} modifier is present, otherwise {@code false}
+     * @see java.lang.reflect.Modifier#STATIC
+     * @see #STATIC
+     */
+    public static boolean isStatic(int modifiers) {
+        return STATIC.matches(modifiers);
+    }
+
+    /**
+     * Checks if the specified modifiers contain the {@code final} modifier.
+     *
+     * @param modifiers the bit mask of modifiers to check
+     * @return {@code true} if the {@code final} modifier is present, otherwise {@code false}
+     * @see java.lang.reflect.Modifier#FINAL
+     * @see #FINAL
+     */
+    public static boolean isFinal(int modifiers) {
+        return FINAL.matches(modifiers);
+    }
+
+    /**
+     * Checks if the specified modifiers contain the {@code synchronized} modifier.
+     *
+     * @param modifiers the bit mask of modifiers to check
+     * @return {@code true} if the {@code synchronized} modifier is present, otherwise {@code false}
+     * @see java.lang.reflect.Modifier#SYNCHRONIZED
+     * @see #SYNCHRONIZED
+     */
+    public static boolean isSynchronized(int modifiers) {
+        return SYNCHRONIZED.matches(modifiers);
+    }
+
+    /**
+     * Checks if the specified modifiers contain the {@code volatile} modifier.
+     *
+     * @param modifiers the bit mask of modifiers to check
+     * @return {@code true} if the {@code volatile} modifier is present, otherwise {@code false}
+     * @see java.lang.reflect.Modifier#VOLATILE
+     * @see #VOLATILE
+     */
+    public static boolean isVolatile(int modifiers) {
+        return VOLATILE.matches(modifiers);
+    }
+
+    /**
+     * Checks if the specified modifiers contain the {@code transient} modifier.
+     *
+     * @param modifiers the bit mask of modifiers to check
+     * @return {@code true} if the {@code transient} modifier is present, otherwise {@code false}
+     * @see java.lang.reflect.Modifier#TRANSIENT
+     * @see #TRANSIENT
+     */
+    public static boolean isTransient(int modifiers) {
+        return TRANSIENT.matches(modifiers);
+    }
+
+    /**
+     * Checks if the specified modifiers contain the {@code native} modifier.
+     *
+     * @param modifiers the bit mask of modifiers to check
+     * @return {@code true} if the {@code native} modifier is present, otherwise {@code false}
+     * @see java.lang.reflect.Modifier#NATIVE
+     * @see #NATIVE
+     */
+    public static boolean isNative(int modifiers) {
+        return NATIVE.matches(modifiers);
+    }
+
+    /**
+     * Checks if the specified modifiers contain the {@code interface} modifier.
+     *
+     * @param modifiers the bit mask of modifiers to check
+     * @return {@code true} if the {@code interface} modifier is present, otherwise {@code false}
+     * @see java.lang.reflect.Modifier#INTERFACE
+     * @see #INTERFACE
+     */
+    public static boolean isInterface(int modifiers) {
+        return INTERFACE.matches(modifiers);
+    }
+
+    /**
+     * Checks if the specified modifiers contain the {@code abstract} modifier.
+     *
+     * @param modifiers the bit mask of modifiers to check
+     * @return {@code true} if the {@code abstract} modifier is present, otherwise {@code false}
+     * @see java.lang.reflect.Modifier#ABSTRACT
+     * @see #ABSTRACT
+     */
+    public static boolean isAbstract(int modifiers) {
+        return ABSTRACT.matches(modifiers);
+    }
+
+    /**
+     * Checks if the specified modifiers contain the {@code strict} modifier.
+     *
+     * @param modifiers the bit mask of modifiers to check
+     * @return {@code true} if the {@code strict} modifier is present, otherwise {@code false}
+     * @see java.lang.reflect.Modifier#STRICT
+     * @see #STRICT
+     */
+    public static boolean isStrict(int modifiers) {
+        return STRICT.matches(modifiers);
+    }
+
+    /**
+     * Checks if the specified modifiers contain the {@code bridge} modifier.
+     *
+     * @param modifiers the bit mask of modifiers to check
+     * @return {@code true} if the {@code bridge} modifier is present, otherwise {@code false}
+     * @see java.lang.reflect.Modifier#BRIDGE
+     * @see #BRIDGE
+     */
+    public static boolean isBridge(int modifiers) {
+        return BRIDGE.matches(modifiers);
+    }
+
+    /**
+     * Checks if the specified modifiers contain the {@code varargs} modifier.
+     *
+     * @param modifiers the bit mask of modifiers to check
+     * @return {@code true} if the {@code varargs} modifier is present, otherwise {@code false}
+     * @see java.lang.reflect.Modifier#VARARGS
+     * @see #VARARGS
+     */
+    public static boolean isVarArgs(int modifiers) {
+        return VARARGS.matches(modifiers);
+    }
+
+    /**
+     * Checks if the specified modifiers contain the {@code synthetic} modifier.
+     *
+     * @param modifiers the bit mask of modifiers to check
+     * @return {@code true} if the {@code synthetic} modifier is present, otherwise {@code false}
+     * @see java.lang.reflect.Modifier#SYNTHETIC
+     * @see #SYNTHETIC
+     */
+    public static boolean isSynthetic(int modifiers) {
+        return SYNTHETIC.matches(modifiers);
+    }
+
+    /**
+     * Checks if the specified modifiers contain the {@code annotation} modifier.
+     *
+     * @param modifiers the bit mask of modifiers to check
+     * @return {@code true} if the {@code annotation} modifier is present, otherwise {@code false}
+     * @see java.lang.reflect.Modifier#ANNOTATION
+     * @see #ANNOTATION
      */
     public static boolean isAnnotation(int modifiers) {
         return ANNOTATION.matches(modifiers);
     }
 
     /**
-     * @param modifiers {@link Class#getModifiers()}
-     * @return true if this class's modifiers represents an enumeration type; false otherwise
-     * @see Class#isEnum()
-     * @see io.microsphere.reflect.Modifier#ENUM
+     * Checks if the specified modifiers contain the {@code enum} modifier.
+     *
+     * @param modifiers the bit mask of modifiers to check
+     * @return {@code true} if the {@code enum} modifier is present, otherwise {@code false}
+     * @see java.lang.reflect.Modifier#ENUM
+     * @see #ENUM
      */
     public static boolean isEnum(int modifiers) {
         return ENUM.matches(modifiers);
     }
 
     /**
-     * @param modifiers {@link Class#getModifiers()}
-     * @return true if this class's modifiers represents a synthetic type; false otherwise
-     * @see Class#isSynthetic()
-     * @see io.microsphere.reflect.Modifier#SYNTHETIC
+     * Checks if the specified modifiers contain the {@code mandated} modifier.
+     *
+     * @param modifiers the bit mask of modifiers to check
+     * @return {@code true} if the {@code mandated} modifier is present, otherwise {@code false}
+     * @see java.lang.reflect.Modifier#MANDATED
+     * @see #MANDATED
      */
-    public static boolean isSynthetic(int modifiers) {
-        return SYNTHETIC.matches(modifiers);
+    public static boolean isMandated(int modifiers) {
+        return MANDATED.matches(modifiers);
     }
 
 }
