c0b0f998f1da36801f3347dc488f8bde7abf460a
Mercy Ma <mercyblitz@gmail.com>
2025-08-05 19:59:21 +0800

    Create MetadataResourceConfigurationPropertyLoader.java

---
+++ microsphere-java-core/src/main/java/io/microsphere/metadata/MetadataResourceConfigurationPropertyLoader.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.metadata;
+
+import io.microsphere.constants.ResourceConstants;
+
+import static io.microsphere.constants.ResourceConstants.CONFIGURATION_PROPERTY_METADATA_RESOURCE;
+
+/**
+ * {@link ConfigurationPropertyLoader} class to load the configuration properties metadata from
+ * {@link ResourceConstants#CONFIGURATION_PROPERTY_METADATA_RESOURCE resource location}.
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see ResourceConstants#CONFIGURATION_PROPERTY_METADATA_RESOURCE
+ * @see ClassPathResourceConfigurationPropertyLoader
+ * @since 1.0.0
+ */
+public class MetadataResourceConfigurationPropertyLoader extends ClassPathResourceConfigurationPropertyLoader {
+
+    public MetadataResourceConfigurationPropertyLoader() {
+        super(CONFIGURATION_PROPERTY_METADATA_RESOURCE);
+    }
+
+    @Override
+    public int getPriority() {
+        return MIN_PRIORITY;
+    }
+}
