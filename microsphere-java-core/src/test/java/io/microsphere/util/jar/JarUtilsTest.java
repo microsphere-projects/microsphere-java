6da6cdcafdeaa7e64d4ebd329ccd3ba7bf79db2a
Mercy Ma <mercyblitz@gmail.com>
2026-02-11 21:45:08 +0800

    Refactor JarUtilsTest and add coverage
    Convert JarUtilsTest to extend AbstractTestCase and replace manual temp-dir handling with createRandomTempDirectory. Clean up imports and use ClassLoaderUtils helpers consistently. Add/adjust tests to improve coverage and robustness: filter behavior, toJarFile edge cases (null, invalid protocol, not-found), assertJarURLProtocol, resolveRelative/absolute paths, findJarEntry, extract variations (URL overload, doExtract with nulls/missing entries) and lambdas for JarEntryFilter. Overall this refactors setup and expands test cases to catch more error conditions and behaviors.

--- microsphere-java-core/src/test/java/io/microsphere/util/jar/JarUtilsTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/util/jar/JarUtilsTest.java
@@ -3,8 +3,8 @@
  */
 package io.microsphere.util.jar;
 
+import io.microsphere.AbstractTestCase;
 import io.microsphere.filter.JarEntryFilter;
-import io.microsphere.util.ClassLoaderUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,23 +12,29 @@ import javax.annotation.Nonnull;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
-import static io.microsphere.io.FileUtils.deleteDirectory;
 import static io.microsphere.net.URLUtils.ofURL;
+import static io.microsphere.util.ClassLoaderUtils.ResourceType.PACKAGE;
 import static io.microsphere.util.ClassLoaderUtils.getClassLoader;
 import static io.microsphere.util.ClassLoaderUtils.getClassResource;
-import static io.microsphere.util.SystemUtils.JAVA_IO_TMPDIR;
+import static io.microsphere.util.ClassLoaderUtils.getResource;
 import static io.microsphere.util.jar.JarUtils.MANIFEST_RESOURCE_PATH;
 import static io.microsphere.util.jar.JarUtils.assertJarURLProtocol;
+import static io.microsphere.util.jar.JarUtils.doExtract;
 import static io.microsphere.util.jar.JarUtils.extract;
+import static io.microsphere.util.jar.JarUtils.filter;
 import static io.microsphere.util.jar.JarUtils.findJarEntry;
 import static io.microsphere.util.jar.JarUtils.resolveJarAbsolutePath;
 import static io.microsphere.util.jar.JarUtils.resolveRelativePath;
 import static io.microsphere.util.jar.JarUtils.toJarFile;
+import static java.util.Collections.emptyList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -39,21 +45,18 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  * @see JarUtilsTest
  * @since 1.0.0
  */
-class JarUtilsTest {
+class JarUtilsTest extends AbstractTestCase {
 
-    private final static File tempDirectory = new File(JAVA_IO_TMPDIR);
-
-    private final static File targetDirectory = new File(tempDirectory, "jar-util-extract");
+    private File targetDirectory;
 
     private final ClassLoader classLoader = getClassLoader(getClass());
 
     private URL resourceURL;
 
     @BeforeEach
-    void setUp() throws IOException {
-        deleteDirectory(targetDirectory);
-        targetDirectory.mkdirs();
-        this.resourceURL = getClassResource(classLoader, Nonnull.class);
+    void setUp() {
+        this.targetDirectory = createRandomTempDirectory();
+        this.resourceURL = getClassResource(this.classLoader, Nonnull.class);
     }
 
     @Test
@@ -63,57 +66,95 @@ class JarUtilsTest {
 
     @Test
     void testAssertJarURLProtocol() {
-        URL url = ofURL("http://localhost");
-        assertThrows(IllegalArgumentException.class, () -> assertJarURLProtocol(url));
+        assertThrows(IllegalArgumentException.class, () -> assertJarURLProtocol(ofURL("http://localhost")));
+        assertJarURLProtocol(ofURL("file://localhost"));
     }
 
     @Test
     void testResolveRelativePath() {
-        String relativePath = resolveRelativePath(resourceURL);
+        String relativePath = resolveRelativePath(this.resourceURL);
         String expectedPath = "javax/annotation/Nonnull.class";
         assertEquals(expectedPath, relativePath);
     }
 
     @Test
-    void testResolveJarAbsolutePath() throws Exception {
-        String jarAbsolutePath = resolveJarAbsolutePath(resourceURL);
+    void testResolveJarAbsolutePath() {
+        String jarAbsolutePath = resolveJarAbsolutePath(this.resourceURL);
         assertNotNull(jarAbsolutePath);
     }
 
     @Test
-    void testToJarFile() throws Exception {
-        JarFile jarFile = toJarFile(resourceURL);
+    void testFilter() throws IOException {
+        JarFile jarFile = toJarFile(this.resourceURL);
+        List<JarEntry> jarEntries = filter(jarFile, null);
+        assertFalse(jarEntries.isEmpty());
+    }
+
+    @Test
+    void testFilterOnNullJarFile() {
+        assertSame(emptyList(), filter(null, null));
+    }
+
+    @Test
+    void testToJarFile() throws IOException {
+        JarFile jarFile = toJarFile(this.resourceURL);
         assertNotNull(jarFile);
     }
 
-    public void testToJarFileOnException() throws Exception {
-        assertThrows(IllegalArgumentException.class, () -> {
-            URL url = new URL("http://www.google.com");
-            JarFile jarFile = toJarFile(url);
-        });
+    @Test
+    void testToJarFileOnNotFound() throws IOException {
+        URL url = new URL("jar:file:/path/to/file.jar!/entry");
+        JarFile jarFile = toJarFile(url);
+        assertNull(jarFile);
+    }
+
+    @Test
+    void testToJarFileOnInvalidProtocol() {
+        assertThrows(IllegalArgumentException.class, () -> toJarFile(new URL("http://github.com")));
+    }
+
+    @Test
+    void testToJarFileOnNPE() {
+        assertThrows(NullPointerException.class, () -> toJarFile(null));
     }
 
     @Test
     void testFindJarEntry() throws Exception {
-        URL resourceURL = getClassResource(classLoader, Nonnull.class);
+        URL resourceURL = getClassResource(this.classLoader, Nonnull.class);
         JarEntry jarEntry = findJarEntry(resourceURL);
         assertNotNull(jarEntry);
     }
 
     @Test
     void testExtract() throws IOException {
-        String jarAbsolutePath = resolveJarAbsolutePath(resourceURL);
-        extract(new File(jarAbsolutePath), targetDirectory);
+        String jarAbsolutePath = resolveJarAbsolutePath(this.resourceURL);
+        extract(new File(jarAbsolutePath), this.targetDirectory);
     }
 
     @Test
     void testExtractWithURL() throws IOException {
-        URL resourceURL = ClassLoaderUtils.getResource(classLoader, ClassLoaderUtils.ResourceType.PACKAGE, "javax.annotation");
-        extract(resourceURL, targetDirectory, new JarEntryFilter() {
-            @Override
-            public boolean accept(JarEntry filteredObject) {
-                return !filteredObject.isDirectory();
-            }
-        });
+        extract(this.resourceURL, this.targetDirectory, (JarEntryFilter) filteredObject -> !filteredObject.isDirectory());
+
+        URL resourceURL = getResource(this.classLoader, PACKAGE, "javax.annotation");
+        extract(resourceURL, this.targetDirectory, (JarEntryFilter) filteredObject -> !filteredObject.isDirectory());
+    }
+
+    @Test
+    void testDoExtractOnNullJarFile() throws IOException {
+        doExtract(null, null, this.targetDirectory);
+    }
+
+    @Test
+    void testDoExtractWithoutJarEntry() throws IOException {
+        JarFile jarFile = toJarFile(this.resourceURL);
+        doExtract(jarFile, null, this.targetDirectory);
+    }
+
+    @Test
+    void testDoExtractOnMissingMatch() throws IOException {
+        URL resourceURL = getClassResource(this.classLoader, Test.class);
+        JarFile jarFile = toJarFile(resourceURL);
+        List<JarEntry> jarEntries = filter(jarFile, null);
+        doExtract(toJarFile(this.resourceURL), jarEntries, this.targetDirectory);
     }
 }
