bb2950c32630e923c052d97e7a9d246a8d6a5819
Mercy Ma <mercyblitz@gmail.com>
2026-02-25 15:51:16 +0800

    Make types API public and add notification()
    Expose types(String...) and add an overload types(Class<?>...) to accept Class inputs (converted to class names). Add a static notification() factory and update notification(...) overloads to delegate to it. Also relax visibility of the types field to allow builder usage. These changes improve the builder's fluent API and make constructing MBeanNotificationInfo instances from Class types more convenient.

--- microsphere-java-core/src/main/java/io/microsphere/management/builder/MBeanNotificationInfoBuilder.java
+++ microsphere-java-core/src/main/java/io/microsphere/management/builder/MBeanNotificationInfoBuilder.java
@@ -40,28 +40,35 @@ import static java.util.stream.Stream.of;
 public class MBeanNotificationInfoBuilder extends MBeanFeatureInfoBuilder<MBeanNotificationInfoBuilder> {
 
     @Nullable
-    private String[] types;
-
-    MBeanNotificationInfoBuilder types(@Nullable String... types) {
-        this.types = types;
-        return this;
-    }
+    String[] types;
 
     MBeanNotificationInfoBuilder() {
         super();
+    }
+
+    public MBeanNotificationInfoBuilder types(@Nullable Class<?>... types) {
+        return types(getClassNames(types));
+    }
+
+    public MBeanNotificationInfoBuilder types(@Nullable String... types) {
+        this.types = types;
+        return this;
     }
 
     public MBeanNotificationInfo build() {
         return new MBeanNotificationInfo(this.types, this.name, this.description, this.descriptor);
     }
 
+    public static MBeanNotificationInfoBuilder notification() {
+        return new MBeanNotificationInfoBuilder();
+    }
+
     public static MBeanNotificationInfoBuilder notification(@Nullable Class<?>... types) {
-        String[] classNames = getClassNames(types);
-        return notification(classNames);
+        return notification().types(types);
     }
 
     public static MBeanNotificationInfoBuilder notification(@Nullable String... types) {
-        return new MBeanNotificationInfoBuilder().types(types);
+        return notification().types(types);
     }
 
     static String[] getClassNames(Class<?>[] types) {
