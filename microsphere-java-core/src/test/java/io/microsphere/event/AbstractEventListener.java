84fb3480fdbb4e5dc219304052f8de5a88855a42
Mercy Ma <mercyblitz@gmail.com>
2026-02-25 19:37:02 +0800

    Make AbstractEventListener extend AbstractTestCase
    Have AbstractEventListener inherit from AbstractTestCase to reuse test utilities. Remove the local static Logger and replace logger.info(...) calls with the inherited log(...) method. Added import for AbstractTestCase and adjusted println accordingly.

--- microsphere-java-core/src/test/java/io/microsphere/event/AbstractEventListener.java
+++ microsphere-java-core/src/test/java/io/microsphere/event/AbstractEventListener.java
@@ -16,15 +16,14 @@
  */
 package io.microsphere.event;
 
+import io.microsphere.AbstractTestCase;
 import io.microsphere.logging.Logger;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static io.microsphere.logging.LoggerFactory.getLogger;
 
-public abstract class AbstractEventListener<E extends Event> implements EventListener<E> {
-
-    private static final Logger logger = getLogger(AbstractEventListener.class);
+public abstract class AbstractEventListener<E extends Event> extends AbstractTestCase implements EventListener<E> {
 
     private final AtomicInteger eventOccurs = new AtomicInteger(0);
 
@@ -41,6 +40,6 @@ public abstract class AbstractEventListener<E extends Event> implements EventLis
     }
 
     protected void println(String message) {
-        logger.info("[{}] {}", Thread.currentThread().getName(), message);
+        log("[{}] {}", Thread.currentThread().getName(), message);
     }
 }
