28413a858271d2611bd1cb5a70d28d96709b499c
Mercy Ma <mercyblitz@gmail.com>
2026-02-25 21:57:04 +0800

    Use Loggable in ThrowableBiConsumerTest
    Replace AbstractTestCase with Loggable as the superclass in ThrowableBiConsumerTest and update the import. This switches the test to use logging utilities provided by Loggable instead of the previous test base.

--- microsphere-java-core/src/test/java/io/microsphere/lang/function/ThrowableBiConsumerTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/lang/function/ThrowableBiConsumerTest.java
@@ -17,7 +17,7 @@
 
 package io.microsphere.lang.function;
 
-import io.microsphere.AbstractTestCase;
+import io.microsphere.Loggable;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -27,7 +27,7 @@ import org.junit.jupiter.api.Test;
  * @see ThrowableBiConsumer
  * @since 1.0.0
  */
-class ThrowableBiConsumerTest extends AbstractTestCase {
+class ThrowableBiConsumerTest extends Loggable {
 
     @Test
     void testAccept() throws Throwable {
