e04cc40452d4115875a7da5bcd0068c8228e01af
Mercy Ma <mercyblitz@gmail.com>
2026-02-25 19:38:25 +0800

    Replace Logger with AbstractTestCase logging
    Extend ShutdownHookUtilsTest from AbstractTestCase and switch logging to the inherited log(...) method. Remove Logger import and static logger field/getLogger usage, updating logger.* calls to log(...). Change inner ShutdownHookCallback from static to instance class so it can use the instance logging. No functional test behavior changed.

--- microsphere-java-core/src/test/java/io/microsphere/util/ShutdownHookUtilsTest.java
+++ microsphere-java-core/src/test/java/io/microsphere/util/ShutdownHookUtilsTest.java
@@ -16,15 +16,14 @@
  */
 package io.microsphere.util;
 
+import io.microsphere.AbstractTestCase;
 import io.microsphere.lang.Prioritized;
-import io.microsphere.logging.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Queue;
 
-import static io.microsphere.logging.LoggerFactory.getLogger;
 import static io.microsphere.util.ShutdownHookUtils.SHUTDOWN_HOOK_CALLBACKS_THREAD_FILTER;
 import static io.microsphere.util.ShutdownHookUtils.addShutdownHookCallback;
 import static io.microsphere.util.ShutdownHookUtils.clearShutdownHookCallbacks;
@@ -44,9 +43,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @since 1.0.0
  */
-class ShutdownHookUtilsTest {
-
-    private static final Logger logger = getLogger(ShutdownHookUtilsTest.class);
+class ShutdownHookUtilsTest extends AbstractTestCase {
 
     @BeforeEach
     void setUp() {
@@ -125,7 +122,7 @@ class ShutdownHookUtilsTest {
                 Thread currentThread = Thread.currentThread();
                 synchronized (currentThread) {
                     try {
-                        logger.info("Thread[name : '{}'] is about to be waited...", currentThread.getName());
+                        log("Thread[name : '{}'] is about to be waited...", currentThread.getName());
                         currentThread.wait();
                         assertTrue(getShutdownHookCallbacks().isEmpty());
                         assertFalse(removeShutdownHookCallback(this));
@@ -140,7 +137,7 @@ class ShutdownHookUtilsTest {
 
         addShutdownHookCallback(() -> {
             synchronized (thread) {
-                logger.info("Thread[name : '{}'] is about to notify a waited Thread[name: '{}']...", Thread.currentThread().getName(), thread.getName());
+                log("Thread[name : '{}'] is about to notify a waited Thread[name: '{}']...", Thread.currentThread().getName(), thread.getName());
                 thread.notify();
             }
         });
@@ -150,7 +147,7 @@ class ShutdownHookUtilsTest {
         thread.start();
     }
 
-    static class ShutdownHookCallback implements Runnable, Prioritized {
+    class ShutdownHookCallback implements Runnable, Prioritized {
 
         private final int priority;
 
@@ -160,13 +157,12 @@ class ShutdownHookUtilsTest {
 
         @Override
         public void run() {
-            logger.trace("Run an instance of ShutdownHookCallback[priority : {}] : {}", priority, this);
+            log("Run an instance of ShutdownHookCallback[priority : {}] : {}", priority, this);
         }
 
         @Override
         public int getPriority() {
             return priority;
         }
-
     }
 }
