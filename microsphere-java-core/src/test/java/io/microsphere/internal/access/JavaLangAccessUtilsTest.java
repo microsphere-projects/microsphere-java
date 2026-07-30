bb7cd0d2d2eeec66df974a4e39c1df3235e2bcd8
Mercy Ma <mercyblitz@gmail.com>
2026-07-30 15:13:50 +0800

    Add JavaLangAccess utility and basic test
    Introduce `JavaLangAccessUtils` to centralize access to JavaLangAccess internals across JDK versions by resolving `sun.misc.JavaLangAccess` first and falling back to `jdk.internal.access.JavaLangAccess`. Add `JavaLangAccessUtilsTest` to verify the class-name constants and resolved class are available.

---
+++ microsphere-java-core/src/test/java/io/microsphere/internal/access/JavaLangAccessUtilsTest.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.internal.access;
+
+
+import org.junit.jupiter.api.Test;
+
+import static io.microsphere.internal.access.JavaLangAccessUtils.JAVA_LANG_ACCESS_CLASS;
+import static io.microsphere.internal.access.JavaLangAccessUtils.JAVA_LANG_ACCESS_CLASS_NAME;
+import static io.microsphere.internal.access.JavaLangAccessUtils.LEGACY_JAVA_LANG_ACCESS_CLASS_NAME;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * {@link JavaLangAccessUtils} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see JavaLangAccessUtils
+ * @since 1.0.0
+ */
+class JavaLangAccessUtilsTest {
+
+    @Test
+    void testConstants() {
+        assertNotNull(LEGACY_JAVA_LANG_ACCESS_CLASS_NAME);
+        assertNotNull(JAVA_LANG_ACCESS_CLASS_NAME);
+        assertNotNull(JAVA_LANG_ACCESS_CLASS);
+    }
+}
